05ebf64e1ab8056f192cd71ce47d9619551588b9
rayrayraykk <18007356109@163.com>
2022-04-26 19:41:16 +0800

    add test for notebooks

---
+++ tests/test_tutorial.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd ..
+
+jupyter nbconvert --to notebook --execute --inplace notebooks/01_quick_start.ipynb
+return=$?
+if [ $return = 1 ];
+then
+    exit 1
+fi
+
+
+jupyter nbconvert --to notebook --execute --inplace notebooks/02_start_your_own_case.ipynb
+return=$?
+if [ $return = 1 ];
+then
+    exit 1
+fi
+
+jupyter nbconvert --to notebook --execute --inplace notebooks/03_personalized_FL.ipynb
+return=$?
+if [ $return = 1 ];
+then
+    exit 1
+else
+    exit 0
+fi
+
