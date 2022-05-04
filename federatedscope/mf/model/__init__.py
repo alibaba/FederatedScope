53d36b872f54f02928d665c8ac64b626e744f59e
DavdGao <102287034+DavdGao@users.noreply.github.com>
2022-05-04 23:00:47 +0800

    Add api ref for mf task and context (#53)
    * add mf api reference and modify README.md

--- federatedscope/mf/model/__init__.py
+++ federatedscope/mf/model/__init__.py
@@ -1,8 +1,9 @@
-from os.path import dirname, basename, isfile, join
-import glob
+from federatedscope.mf.model.model import BasicMFNet, VMFNet, HMFNet
+from federatedscope.mf.model.model_builder import get_mfnet
 
-modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [
-    basename(f)[:-3] for f in modules
-    if isfile(f) and not f.endswith('__init__.py')
+    "get_mfnet",
+    "BasicMFNet",
+    "VMFNet",
+    "HMFNet"
 ]
