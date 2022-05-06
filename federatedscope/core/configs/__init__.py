c72a8a5d32bbfd052105f8508726b432b1791dda
yxdyc <67475544+yxdyc@users.noreply.github.com>
2022-05-06 19:49:34 +0800

    fixed some missing API reference in fs.core (#54)
    As the title says.

--- federatedscope/core/configs/__init__.py
+++ federatedscope/core/configs/__init__.py
@@ -13,6 +13,14 @@ all_sub_configs = copy.copy(__all__)
 if "config" in all_sub_configs:
     all_sub_configs.remove('config')
 
+
+from federatedscope.core.configs.config import CN, init_global_cfg
+__all__ = __all__ + \
+          [
+              'CN',
+              'init_global_cfg'
+          ]
+
 # reorder the config to ensure the base config will be registered first
 base_configs = [
     'cfg_data', 'cfg_fl_setting', 'cfg_model', 'cfg_training', 'cfg_evaluation'
@@ -20,3 +28,4 @@ base_configs = [
 for base_config in base_configs:
     all_sub_configs.pop(all_sub_configs.index(base_config))
     all_sub_configs.insert(0, base_config)
+
--- federatedscope/mf/dataloader/__init__.py
+++ federatedscope/mf/dataloader/__init__.py
@@ -1,6 +1,3 @@
 from federatedscope.mf.dataloader.dataloader import load_mf_dataset, MFDataLoader
 
-__all__ = [
-    'load_mf_dataset',
-    'MFDataLoader'
-]
+__all__ = ['load_mf_dataset', 'MFDataLoader']
