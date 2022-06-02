337c5b94b2b681fd4dcf9695232c972c1e9d9fba
gaodawei.gdw <gaodawei.gdw@alibaba-inc.com>
2022-06-02 10:58:43 +0800

    try to test the difference between "batch" and "epoch"

--- scripts/contest_exp_scripts/run_contest_fedavg.sh
+++ scripts/contest_exp_scripts/run_contest_fedavg.sh
@@ -22,7 +22,7 @@ for (( i=0; i<${#lrs[@]}; i++ ))
 do
     for (( j=0; j<${#local_updates[@]}; j++ ))
     do
-        log=${outdir}/gin_lr-${lrs[$i]}_step-${local_updates[$j]}_on_${dataset}.log
+        log=${outdir}/gin_lr-${lrs[$i]}_epoch-${local_updates[$j]}_on_${dataset}.log
         for k in {1..3}
         do
             python federatedscope/main.py --cfg scripts/contest_exp_scripts/fedavg_gnn_minibatch_on_multi_task.yaml \
@@ -31,6 +31,7 @@ do
             data.type ${dataset} \
             optimizer.lr ${lrs[$i]} \
             federate.local_update_steps ${local_updates[$j]} \
+            federate.batch_or_epoch 'epoch' \
             seed $k >>${log} 2>&1
         done
         python federatedscope/parse_exp_results.py --input ${log}
