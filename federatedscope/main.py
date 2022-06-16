13df0e5d6131161f403dde47e1ab5adca652cd2a
cheneydon <cheneychenhe98@gmail.com>
2022-06-16 15:12:57 +0800

    recover main.py

--- federatedscope/main.py
+++ federatedscope/main.py
@@ -6,33 +6,37 @@ if DEV_MODE:
     file_dir = os.path.join(os.path.dirname(__file__), '..')
     sys.path.append(file_dir)
 
-from yacs.config import CfgNode
 from federatedscope.core.cmd_args import parse_args
 from federatedscope.core.auxiliaries.data_builder import get_data
 from federatedscope.core.auxiliaries.utils import setup_seed, update_logger
 from federatedscope.core.auxiliaries.worker_builder import get_client_cls, get_server_cls
 from federatedscope.core.configs.config import global_cfg
 from federatedscope.core.fed_runner import FedRunner
+from yacs.config import CfgNode
 
 if os.environ.get('https_proxy'):
     del os.environ['https_proxy']
 if os.environ.get('http_proxy'):
     del os.environ['http_proxy']
 
-
 if __name__ == '__main__':
     init_cfg = global_cfg.clone()
     args = parse_args()
     init_cfg.merge_from_file(args.cfg_file)
     init_cfg.merge_from_list(args.opts)
+
     update_logger(init_cfg)
     setup_seed(init_cfg.seed)
 
+    # federated dataset might change the number of clients
+    # thus, we allow the creation procedure of dataset to modify the global cfg object
     data, modified_cfg = get_data(config=init_cfg.clone())
     init_cfg.merge_from_other_cfg(modified_cfg)
+
     init_cfg.freeze()
 
     # allow different settings for different clients
+    # cfg_client.merge_from_file(args.cfg_client)
     if args.cfg_client is None:
         cfg_client = None
     else:
