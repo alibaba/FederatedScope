bd44d3d4784be63cca0d9d9c04d83d9d95f32b4e
yxdyc <67475544+yxdyc@users.noreply.github.com>
2022-06-17 14:18:17 +0800

    Update pFL-Bench scripts and README (#169)

--- benchmark/pFL-Bench/res_analysis_plot/render_paper_res.py
+++ benchmark/pFL-Bench/res_analysis_plot/render_paper_res.py
@@ -1,7 +1,12 @@
+import copy
+import json
+import os
 
-
+import numpy as np
 import wandb
 from collections import OrderedDict
+
+import yaml
 
 api = wandb.Api()
 
@@ -224,6 +229,10 @@ expected_datasets_name = ["cola", "sst2", "pubmed", "cora", "citeseer", "cifar10
 expected_seed_set = ["1", "2", "3"]
 expected_expname_tag = set()
 
+original_method_names = [
+    "Global-Train", "Isolated", "FedAvg", "pFedMe", "FedBN", "Ditto", "FedEM"
+]
+
 for method_name in expected_method_names:
     for dataset_name in expected_datasets_name:
         for seed in expected_seed_set:
@@ -243,8 +252,7 @@ for expname_tag in expected_expname_tag:
             all_res_structed[expname_tag][metric] = "-"
 
 
-
-def check_run_stats(filter_seed_set=None):
+def load_best_repeat_res(filter_seed_set=None):
     for expname_tag in expected_expname_tag:
         filter = {"$and":
             [
@@ -269,7 +277,10 @@ def check_run_stats(filter_seed_set=None):
                                 continue
 
                         if metric == "participation_gap":
-                            all_res_structed[expname_tag][metric] = all_res_structed[expname_tag]["best_unseen_client_summarized_weighted_avg_unseen/test_acc"] - all_res_structed[expname_tag]["best_client_summarized_weighted_avg/test_acc"]
+                            all_res_structed[expname_tag][metric] = all_res_structed[expname_tag][
+                                                                        "best_unseen_client_summarized_weighted_avg_unseen/test_acc"] - \
+                                                                    all_res_structed[expname_tag][
+                                                                        "best_client_summarized_weighted_avg/test_acc"]
                         else:
                             all_res_structed[expname_tag][metric] = run.summary[metric]
                     except KeyError:
@@ -301,7 +312,6 @@ def check_run_stats(filter_seed_set=None):
         print()
 
 
-
 def bytes_to_unit_size(size_bytes):
     import math
     if size_bytes == 0:
@@ -312,20 +322,24 @@ def bytes_to_unit_size(size_bytes):
     s = round(size_bytes / p, 2)
     return f"{s}{size_name[i]}"
 
+
 def unit_size_to_bytes(size_str):
     if not isinstance(size_str, str):
         return size_str
     else:
-        last_unit = size_str[-1]
-        size_name = ("", "K", "M", "G", "T", "P", "E", "Z", "Y")
-        if last_unit not in size_name:
-            return float(size_str)
-        else:
-            # need transform
-            import math
-            idx = size_name.index(last_unit)
-            p = math.pow(1024, idx)
-            return float(size_str[:-1]) * p
+        try:
+            last_unit = size_str[-1]
+            size_name = ("", "K", "M", "G", "T", "P", "E", "Z", "Y")
+            if last_unit not in size_name:
+                return float(size_str)
+            else:
+                # need transform
+                import math
+                idx = size_name.index(last_unit)
+                p = math.pow(1024, idx)
+                return float(size_str[:-1]) * p
+        except:
+            return size_str
 
 
 def avg_res_of_seeds():
@@ -335,7 +349,8 @@ def avg_res_of_seeds():
             continue
         else:
             for metric in column_names_generalization + column_names_efficiency + column_names_fair:
-                if all_res_structed[expname_tag][metric] == "-" and "Global" not in expname_tag and "Isolated" not in expname_tag:
+                if all_res_structed[expname_tag][
+                    metric] == "-" and "Global" not in expname_tag and "Isolated" not in expname_tag:
                     print(f"missing {expname_tag} for metric {metric}")
                 method, dataname, seed = expname_tag.split("_")
                 cur_res = all_res_structed[expname_tag][metric]
@@ -353,7 +368,66 @@ def avg_res_of_seeds():
                         res = bytes_to_unit_size(res)
                     all_res_structed[expname_tag][metric] = res
 
-def print_paper_table(filters_each_line_table):
+
+def highlight_tex_res_in_table(res_to_print_matrix_raw, rank_order, need_scale=False, filter_out=None,
+                               convergence_case=False):
+    res_to_print_matrix = []
+    if filter_out is not None:
+        # filter out the Global-Train and Isolated
+        for line in res_to_print_matrix_raw:
+            if line[0] in filter_out:
+                continue
+            else:
+                res_to_print_matrix.append(line)
+    else:
+        res_to_print_matrix = res_to_print_matrix_raw
+
+    res_np = np.array(res_to_print_matrix)
+    row_len, col_len = res_np.shape
+
+    if need_scale:
+        vfun = np.vectorize(unit_size_to_bytes)
+        res_np = vfun(res_np)
+
+    # select method idx
+    method_heads_all = res_np[:, :1]
+    selected_method_idx = [i for i in range(row_len) if method_heads_all[i] in original_method_names]
+
+    raw_i_to_selected_i = {}
+    for idx_i, selected_i in enumerate(selected_method_idx):
+        raw_i_to_selected_i[selected_i] = idx_i
+
+    # render column by column
+    for col_i, col in enumerate(res_np[:, 1:].T):
+        # first replace the missing results into numerical res
+        if rank_order[col_i] == "+":
+            # order == "+" indicates the larger, the better
+            col = np.where(col == "-", -999999999, col)
+        else:
+            col = np.where(col == "-", 9999999999, col)
+        if convergence_case:
+            col = np.where(col == "0", 9999999999, col)
+        col = col.astype("float")
+        if rank_order[col_i] == "+":
+            col = - col
+        col_all = pd.DataFrame(col)
+        ind_all_method = col_all.rank(method='dense').astype(int)[0].values.tolist()
+        col_filter = pd.DataFrame(col[selected_method_idx])
+        ind_partial_method_tmp = col_filter.rank(method='dense').astype(int)[0].values.tolist()
+        for raw_i in range(row_len):
+            if ind_all_method[raw_i] == 1:
+                res_to_print_matrix[raw_i][col_i + 1] = "\\textbf{" + res_to_print_matrix[raw_i][col_i + 1] + "}"
+            if ind_all_method[raw_i] == 2:
+                res_to_print_matrix[raw_i][col_i + 1] = "\\underline{" + res_to_print_matrix[raw_i][col_i + 1] + "}"
+            if raw_i in selected_method_idx and ind_partial_method_tmp[raw_i_to_selected_i[raw_i]] == 1:
+                res_to_print_matrix[raw_i][col_i + 1] = "\\color{red}{" + res_to_print_matrix[raw_i][col_i + 1] + "}"
+            if raw_i in selected_method_idx and ind_partial_method_tmp[raw_i_to_selected_i[raw_i]] == 2:
+                res_to_print_matrix[raw_i][col_i + 1] = "\\color{blue}{" + res_to_print_matrix[raw_i][col_i + 1] + "}"
+
+    return res_to_print_matrix
+
+
+def print_paper_table_from_repeat(filters_each_line_table):
     res_of_each_line_generalization = OrderedDict()
     res_of_each_line_fair = OrderedDict()
     res_of_each_line_efficiency = OrderedDict()
@@ -379,25 +453,46 @@ def print_paper_table(filters_each_line_table):
     print("\n=============res_of_each_line [Generalization]===============" + ",".join(
         list(filters_each_line_table.keys())))
     # Acc, Unseen-ACC, Delta
+    res_to_print_matrix = []
     for key in expected_method_names:
-        res_to_print = ["{:.2f}".format(v * 100) if v != "-" else v for v in     res_of_each_line_generalization[key]]
+        res_to_print = ["{:.2f}".format(v * 100) if v != "-" else v for v in res_of_each_line_generalization[key]]
         res_to_print = [key] + res_to_print
-        print(",".join(res_to_print))
-    
-    print("\n=============res_of_each_line [Fairness]===============" + ",".join(list(    filters_each_line_table.keys())))
+        res_to_print_matrix.append(res_to_print)
+        # print("&".join(res_to_print) + "\\\\")
+
+    colum_order_per_data = ["+", "+", "+"]
+    # "+" indicates the larger, the better
+    rank_order = colum_order_per_data * len(filters_each_line_table)
+    res_to_print_matrix = highlight_tex_res_in_table(res_to_print_matrix, rank_order=rank_order)
+    for res_to_print in res_to_print_matrix:
+        print("&".join(res_to_print) + "\\\\")
+
+    print("\n=============res_of_each_line [Fairness]===============" + ",".join(list(filters_each_line_table.keys())))
+    res_to_print_matrix = []
     for key in expected_method_names:
-        res_to_print = ["{:.2f}".format(v * 100) if v != "-" else v for v in     res_of_each_line_fair[key]]
+        res_to_print = ["{:.2f}".format(v * 100) if v != "-" else v for v in res_of_each_line_fair[key]]
         res_to_print = [key] + res_to_print
-        print(",".join(res_to_print))
-    print("\n=============res_of_each_line [All Efficiency]===============" + ",".join(
+        res_to_print_matrix.append(res_to_print)
+        # print("&".join(res_to_print) + "\\\\")
+
+    colum_order_per_data = ["+", "-", "+"]
+    # "+" indicates the larger, the better
+    rank_order = colum_order_per_data * len(filters_each_line_table)
+    res_to_print_matrix = highlight_tex_res_in_table(res_to_print_matrix, rank_order=rank_order, filter_out=["Global-Train"])
+    for res_to_print in res_to_print_matrix:
+        print("&".join(res_to_print) + "\\\\")
+
+    # print("\n=============res_of_each_line [All Efficiency]===============" + ",".join(
+    #    list(filters_each_line_table.keys())))
+    ## FLOPS, UPLOAD, DOWNLOAD
+    # for key in expected_method_names:
+    #    res_to_print = [str(v) for v in res_of_each_line_efficiency[key]]
+    #    res_to_print = [key] + res_to_print
+    #    print("&".join(res_to_print) + "\\\\")
+
+    print("\n=============res_of_each_line [flops, communication, acc]===============" + ",".join(
         list(filters_each_line_table.keys())))
-    # FLOPS, UPLOAD, DOWNLOAD
-    for key in expected_method_names:
-        res_to_print = [str(v) for v in res_of_each_line_efficiency[key]]
-        res_to_print = [key] + res_to_print
-        print(",".join(res_to_print))
-    print("\n=============res_of_each_line [flops, communication, acc]===============" +     ",".join(
-        list(filters_each_line_table.keys())))
+    res_to_print_matrix = []
     for key in expected_method_names:
         res_of_each_line_commu_acc_trade[key] = []
         dataset_num = 2 if "cola" in list(filters_each_line_table.keys()) else 3
@@ -405,15 +500,26 @@ def print_paper_table(filters_each_line_table):
             res_of_each_line_commu_acc_trade[key].extend(
                 [str(res_of_each_line_efficiency[key][i * 4])] + \
                 [str(res_of_each_line_efficiency[key][i * 4 + 1])] + \
-                ["{:.2f}".format(v * 100) if v != "-" else v for v in     res_of_each_line_generalization[key][i * 3:i * 3 + 1]]
+                ["{:.2f}".format(v * 100) if v != "-" else v for v in
+                 res_of_each_line_generalization[key][i * 3:i * 3 + 1]]
             )
-    
+
         res_to_print = [str(v) for v in res_of_each_line_commu_acc_trade[key]]
         res_to_print = [key] + res_to_print
-        print(",".join(res_to_print))
+        res_to_print_matrix.append(res_to_print)
+        # print("&".join(res_to_print)+ "\\\\")
+
+    colum_order_per_data = ["-", "-", "+"]
+    # "+" indicates the larger, the better
+    rank_order = colum_order_per_data * len(filters_each_line_table)
+    res_to_print_matrix = highlight_tex_res_in_table(res_to_print_matrix, rank_order=rank_order, need_scale=True,
+                                                     filter_out=["Global-Train", "Isolated"])
+    for res_to_print in res_to_print_matrix:
+        print("&".join(res_to_print) + "\\\\")
 
     print("\n=============res_of_each_line [converge_round, acc]===============" + ",".join(
         list(filters_each_line_table.keys())))
+    res_to_print_matrix = []
     for key in expected_method_names:
         res_of_each_line_conver_acc_trade[key] = []
         dataset_num = 2 if "cola" in list(filters_each_line_table.keys()) else 3
@@ -422,13 +528,24 @@ def print_paper_table(filters_each_line_table):
                 [str(res_of_each_line_efficiency[key][i * 4 + 3])] + \
                 # [str(res_of_each_line_efficiency[key][i * 4 + 4])] + \
                 ["{:.2f}".format(v * 100) if v != "-" else v for v in res_of_each_line_fair[key][i * 3:i * 3 + 1]]
-        )
+            )
 
         res_to_print = [str(v) for v in res_of_each_line_conver_acc_trade[key]]
         res_to_print = [key] + res_to_print
-        print(",".join(res_to_print))
+        res_to_print_matrix.append(res_to_print)
+        # print("&".join(res_to_print) + "\\\\")
+
+    colum_order_per_data = ["-", "+"]
+    # "+" indicates the larger, the better
+    rank_order = colum_order_per_data * len(filters_each_line_table)
+    res_to_print_matrix = highlight_tex_res_in_table(res_to_print_matrix, rank_order=rank_order,
+                                                     filter_out=["Global-Train", "Isolated"], convergence_case=True)
+    for res_to_print in res_to_print_matrix:
+        print("&".join(res_to_print) + "\\\\")
+
 
 import json
+
 with open('best_res_all_metric.json', 'r') as fp:
     all_res_structed_load = json.load(fp)
     for expname_tag in expected_expname_tag:
@@ -437,9 +554,9 @@ with open('best_res_all_metric.json', 'r') as fp:
         for metric in column_names_generalization + column_names_efficiency + column_names_fair:
             all_res_structed[expname_tag][metric] = all_res_structed_load[expname_tag][metric]
 
-
 # add all res to a df
 import pandas as pd
+
 
 def load_data_to_pd(use_repeat_res=False):
     all_res_for_pd = []
@@ -463,16 +580,17 @@ def load_data_to_pd(use_repeat_res=False):
             alpha = float(res[1].replace("cifar10-alpha", ""))
         res.append(s)
         res.append(alpha)
-        total_com_bytes = unit_size_to_bytes(res[-5]) +  unit_size_to_bytes(res[-4])
+        total_com_bytes = unit_size_to_bytes(res[-5]) + unit_size_to_bytes(res[-4])
         total_flops = unit_size_to_bytes(res[-6])
         res.append(total_com_bytes)
         res.append(total_flops)
         all_res_for_pd.append(res)
 
-    all_res_pd = pd.DataFrame().from_records(all_res_for_pd, columns=["method", "data", "seed"] + column_names_generalization + column_names_fair + column_names_efficiency + ["s", "alpha", "communication_bytes", "total_flops"])
+    all_res_pd = pd.DataFrame().from_records(all_res_for_pd, columns=["method", "data",
+                                                                      "seed"] + column_names_generalization + column_names_fair + column_names_efficiency + [
+                                                                         "s", "alpha", "communication_bytes",
+                                                                         "total_flops"])
     return all_res_pd
-
-
 
 
 def plot_generalization_lines(all_res_pd, data_cate, data_cate_name):
@@ -482,10 +600,8 @@ def plot_generalization_lines(all_res_pd, data_cate, data_cate_name):
 
     plt.clf()
     sns.set()
-    #fig, axes = plt.subplots(1, 3, figsize=(6, 4))
     fig, axes = plt.subplots(1, 3, figsize=(6, 4))
     print(all_res_pd.columns.tolist())
-
 
     plot_data = all_res_pd.loc[all_res_pd["data"].isin(data_cate)]
 
@@ -525,7 +641,6 @@ def plot_generalization_lines(all_res_pd, data_cate, data_cate_name):
         plt.show()
 
 
-
 def plot_tradeoff(all_res_pd, data_cate, data_cate_name, metric_a, metric_b, fig_time):
     import seaborn as sns
     from matplotlib import pyplot as plt
@@ -555,7 +670,7 @@ def plot_tradeoff(all_res_pd, data_cate, data_cate_name, metric_a, metric_b, fig
         s=100
     )
     ax.set(xlabel=column_name_for_plot[metric_a], ylabel=column_name_for_plot[metric_b])
-    #plt.gca().invert_xaxis()
+    # plt.gca().invert_xaxis()
     if metric_a == "total_flops":
         ax.set_xscale('log')
 
@@ -568,31 +683,35 @@ def plot_tradeoff(all_res_pd, data_cate, data_cate_name, metric_a, metric_b, fig
 
     plt.show()
 
+if __name__ == "__main__":
+    load_best_repeat_res(["1", "repeat"])
+    avg_res_of_seeds()
+
+    print_paper_table_from_repeat(filters_each_line_main_table)
+    print_paper_table_from_repeat(filters_each_line_femnist_all_s)
+    print_paper_table_from_repeat(filters_each_line_all_cifar10)
+    print_paper_table_from_repeat(filters_each_line_all_nlp)
+    print_paper_table_from_repeat(filters_each_line_all_graph)
+
+    all_res_pd = load_data_to_pd(use_repeat_res=False)
+    all_res_pd_repeat = load_data_to_pd(use_repeat_res=True)
 
 
-check_run_stats(["1", "repeat"])
-avg_res_of_seeds()
-
-print_paper_table(filters_each_line_main_table)
-print_paper_table(filters_each_line_femnist_all_s)
-print_paper_table(filters_each_line_all_cifar10)
-print_paper_table(filters_each_line_all_nlp)
-print_paper_table(filters_each_line_all_graph)
-
-# plot line figures
-all_res_pd = load_data_to_pd(use_repeat_res=False)
-all_res_pd_repeat = load_data_to_pd(use_repeat_res=True)
-plot_generalization_lines(all_res_pd, list( filters_each_line_femnist_all_s.keys()), data_cate_name="femnist_all")
-plot_generalization_lines(all_res_pd, list(filters_each_line_all_cifar10.keys()), data_cate_name="cifar10_all")
-
-# plot trade-off figs
-for data_name in list(filters_each_line_main_table.keys()):
-    plot_tradeoff(all_res_pd_repeat, [data_name], data_cate_name=data_name, metric_a="communication_bytes", metric_b="best_client_summarized_weighted_avg/test_acc", fig_time="com-acc")
-
-for data_name in list(filters_each_line_main_table.keys()):
-    plot_tradeoff(all_res_pd_repeat, [data_name], data_cate_name=data_name, metric_a="total_flops", metric_b="best_client_summarized_weighted_avg/test_acc", fig_time="flops-acc")
-
-for data_name in list(filters_each_line_main_table.keys()):
-    plot_tradeoff(all_res_pd_repeat, [data_name], data_cate_name=data_name, metric_a="sys_avg/global_convergence_round", metric_b="best_client_summarized_weighted_avg/test_acc", fig_time="round-acc")
+def plot_line_figs():
+    plot_generalization_lines(all_res_pd, list(filters_each_line_femnist_all_s.keys()), data_cate_name="femnist_all")
+    plot_generalization_lines(all_res_pd, list(filters_each_line_all_cifar10.keys()), data_cate_name="cifar10_all")
 
 
+def plot_trade_off_figs(filters_each_line_main_table):
+    for data_name in list(filters_each_line_main_table.keys()):
+        plot_tradeoff(all_res_pd_repeat, [data_name], data_cate_name=data_name, metric_a="communication_bytes",
+                      metric_b="best_client_summarized_weighted_avg/test_acc", fig_time="com-acc")
+
+    for data_name in list(filters_each_line_main_table.keys()):
+        plot_tradeoff(all_res_pd_repeat, [data_name], data_cate_name=data_name, metric_a="total_flops",
+                      metric_b="best_client_summarized_weighted_avg/test_acc", fig_time="flops-acc")
+
+    for data_name in list(filters_each_line_main_table.keys()):
+        plot_tradeoff(all_res_pd_repeat, [data_name], data_cate_name=data_name,
+                      metric_a="sys_avg/global_convergence_round",
+                      metric_b="best_client_summarized_weighted_avg/test_acc", fig_time="round-acc")
