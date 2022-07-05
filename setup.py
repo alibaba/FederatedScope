ceebcd8c1637585698464540a3872316c194a0fe
rayrayraykk <18007356109@163.com>
2022-07-05 17:26:15 +0800

    update requires

--- setup.py
+++ setup.py
@@ -56,11 +56,9 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.9',
 )
