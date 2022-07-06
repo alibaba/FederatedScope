6fddb390d6f68139ca473c32d74564f41a3022f2
joneswong <joneswong.ml@gmail.com>
2022-07-06 11:45:22 +0800

    for toy lr

--- scripts/run_femnist_on_cloud.sh
+++ scripts/run_femnist_on_cloud.sh
@@ -2,18 +2,15 @@ set -e
 
 NODE=$1
 
-echo "Test distributed mode with LR..."
-
-echo "Data generation"
-python scripts/gen_data.py
+echo "Test distributed mode with ConvNet..."
 
 if [ $NODE -eq 0 ]
 then
   python federatedscope/main.py --cfg federatedscope/example_configs/distributed_femnist_server.yaml distribute.server_host 172.17.138.149 &
   sleep 2
-  python federatedscope/main.py --cfg federatedscope/example_configs/distributed_femnist_client_1.yaml distribute.server_host 172.17.138.149 distribute.client_host 172.17.138.149 distribute.client_port 50052 &
+  python federatedscope/main.py --cfg federatedscope/example_configs/distributed_femnist_client1.yaml distribute.server_host 172.17.138.149 distribute.client_host 172.17.138.149 distribute.client_port 50052 &
 else
   python federatedscope/main.py --cfg federatedscope/example_configs/distributed_femnist_client2.yaml distribute.server_host 172.17.138.149 distribute.server_port 50051 distribute.client_host 172.17.138.148 distribute.client_port 50053 &
   sleep 2
-  python federatedscope/main.py --cfg federatedscope/example_configs/distributed_femnist_client2.yaml distribute.server_host 172.17.138.149 distribute.server_port 50051 distribute.client_host 172.17.138.148 distribute.client_port 50054 &
+  python federatedscope/main.py --cfg federatedscope/example_configs/distributed_femnist_client3.yaml distribute.server_host 172.17.138.149 distribute.server_port 50051 distribute.client_host 172.17.138.148 distribute.client_port 50054 &
 fi
