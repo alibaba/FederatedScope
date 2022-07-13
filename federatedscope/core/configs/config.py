08ecf81fd4159977150d3c97ff9d4aca97a62549
道辕 <daoyuanchen.cdy@alibaba-inc.com>
2022-07-13 19:40:33 +0800

    support int and float conversation in cfg

--- federatedscope/core/configs/config.py
+++ federatedscope/core/configs/config.py
@@ -2,13 +2,105 @@ import copy
 import logging
 import os
 
-from yacs.config import CfgNode
-from yacs.config import _assert_with_logging
-from yacs.config import _check_and_coerce_cfg_value_type
+from yacs.config import CfgNode, _assert_with_logging, _VALID_TYPES
 
 import federatedscope.register as register
 
 logger = logging.getLogger(__name__)
+
+# allow int <-> float conversation
+casts = [(tuple, list), (list, tuple), (int, float), (float, int)]
+
+
+def check_and_coerce_cfg_value_type(replacement, original, key, full_key):
+    """
+        [Modified from yacs, to allow int <-> float conversation]
+
+    Checks that `replacement`, which is intended to replace `original` is of
+    the right type. The type is correct if it matches exactly or is one of a
+    few cases in which the type can be easily coerced.
+    """
+    original_type = type(original)
+    replacement_type = type(replacement)
+
+    # The types must match (with some exceptions)
+    if replacement_type == original_type:
+        return replacement
+
+    # If either of them is None,
+    # allow type conversion to one of the valid types
+    if (replacement_type is None and original_type in _VALID_TYPES) or (
+            original_type is None and replacement_type in _VALID_TYPES):
+        return replacement
+
+    # Cast replacement from from_type to to_type
+    # if the replacement and original types match from_type and to_type
+    def conditional_cast(from_type, to_type):
+        if replacement_type == from_type and original_type == to_type:
+            return True, to_type(replacement)
+        else:
+            return False, None
+
+    # Conditionally casts
+    # list <-> tuple
+    # For py2: allow converting from str (bytes) to a unicode string
+    try:
+        casts.append((str, unicode))  # noqa: F821
+    except Exception:
+        pass
+
+    for (from_type, to_type) in casts:
+        converted, converted_value = conditional_cast(from_type, to_type)
+        if converted:
+            return converted_value
+
+    raise ValueError(
+        "Type mismatch ({} vs. {}) with values ({} vs. {}) for config "
+        "key: {}".format(original_type, replacement_type, original,
+                         replacement, full_key))
+
+
+def merge_dict_a_into_b(a, b, root, key_list):
+    """
+        [Modified from yacs, to allow int <-> float conversation]
+
+    Merge config dictionary a into config dictionary b, clobbering the
+    options in b whenever they are also specified in a.
+    """
+    _assert_with_logging(
+        isinstance(a, CfgNode),
+        "`a` (cur type {}) must be an instance of {}".format(type(a), CfgNode),
+    )
+    _assert_with_logging(
+        isinstance(b, CfgNode),
+        "`b` (cur type {}) must be an instance of {}".format(type(b), CfgNode),
+    )
+
+    for k, v_ in a.items():
+        full_key = ".".join(key_list + [k])
+
+        v = copy.deepcopy(v_)
+        v = b._decode_cfg_value(v)
+
+        if k in b:
+            v = check_and_coerce_cfg_value_type(v, b[k], k, full_key)
+            # Recursively merge dicts
+            if isinstance(v, CfgNode):
+                try:
+                    merge_dict_a_into_b(v, b[k], root, key_list + [k])
+                except BaseException:
+                    raise
+            else:
+                b[k] = v
+        elif b.is_new_allowed():
+            b[k] = v
+        else:
+            if root.key_is_deprecated(full_key):
+                continue
+            elif root.key_is_renamed(full_key):
+                root.raise_key_rename_error(full_key)
+            else:
+                raise KeyError("Non-existent config key: {}".format(full_key))
 
 
 class CN(CfgNode):
@@ -41,7 +133,11 @@ class CN(CfgNode):
         :param cfg_filename (string):
         :return:
         """
-        super(CN, self).merge_from_file(cfg_filename)
+        cfg_check_funcs = copy.copy(self.cfg_check_funcs)
+        with open(cfg_filename, "r") as f:
+            cfg = self.load_cfg(f)
+        self.merge_from_other_cfg(cfg)
+        self.cfg_check_funcs = cfg_check_funcs
         self.assert_cfg()
 
     def merge_from_other_cfg(self, cfg_other):
@@ -51,7 +147,10 @@ class CN(CfgNode):
         :param cfg_other (CN):
         :return:
         """
-        super(CN, self).merge_from_other_cfg(cfg_other)
+
+        cfg_check_funcs = copy.copy(self.cfg_check_funcs)
+        merge_dict_a_into_b(cfg_other, self, self, [])
+        self.cfg_check_funcs = cfg_check_funcs
         self.assert_cfg()
 
     def merge_from_list(self, cfg_list):
@@ -62,6 +161,15 @@ class CN(CfgNode):
 
         :param cfg_list (list):
         :return:
+        """
+        cfg_check_funcs = copy.copy(self.cfg_check_funcs)
+        self.merge_from_list_yacs(cfg_list)
+        self.cfg_check_funcs = cfg_check_funcs
+        self.assert_cfg()
+
+    def merge_from_list_yacs(self, cfg_list):
+        """Merge config (keys, values) in a list (e.g., from command line) into
+        this CfgNode. For example, `cfg_list = ['FOO.BAR', 0.5]`.
         """
         _assert_with_logging(
             len(cfg_list) % 2 == 0,
@@ -85,11 +193,9 @@ class CN(CfgNode):
                                  "Non-existent key: {}".format(full_key))
             value = self._decode_cfg_value(v)
             if subkey in d:
-                value = _check_and_coerce_cfg_value_type(
+                value = check_and_coerce_cfg_value_type(
                     value, d[subkey], subkey, full_key)
             d[subkey] = value
-
-        self.assert_cfg()
 
     def assert_cfg(self):
         """
@@ -140,14 +246,13 @@ class CN(CfgNode):
                 # update the frozen config
                 try:
                     import wandb
+                    import yaml
+                    cfg_yaml = yaml.safe_load(tmp_cfg.dump())
+                    wandb.config.update(cfg_yaml, allow_val_change=True)
                 except ImportError:
                     logger.error(
                         "cfg.wandb.use=True but not install the wandb package")
                     exit()
-
-                import yaml
-                cfg_yaml = yaml.safe_load(tmp_cfg.dump())
-                wandb.config.update(cfg_yaml, allow_val_change=True)
 
         if inform:
             logger.info("the used configs are: \n" + str(tmp_cfg))
