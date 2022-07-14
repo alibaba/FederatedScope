2d5489df7e84c4d22f15f2e6287f3a6e67b9fa46
rayrayraykk <18007356109@163.com>
2022-07-14 18:26:18 +0800

    add config

--- federatedscope/organizer/client.py
+++ federatedscope/organizer/client.py
@@ -1,7 +1,12 @@
-import time
-from server import sendmail
-result = sendmail.delay(dict(to='celery@python.org'))
-while not result.ready():
-    print('Waiting for response... (will re-try in 0.1s)')
-    time.sleep(0.1)
-print(result.get(timeout=1))
+# import time
+# from server import sendmail
+# result = sendmail.delay(dict(to='celery@python.org'))
+# while not result.ready():
+#     print('Waiting for response... (will re-try in 0.1s)')
+#     time.sleep(0.1)
+# print(result.get(timeout=1))
+
+from celery import Celery
+celery = Celery()
+celery.config_from_object('client_config')
+celery.send_task('organizer.sendmail', dict(to='celery@python.org'))
---
+++ federatedscope/organizer/server_config.py
@@ -0,0 +1,5 @@
+task_serializer = 'json'
+result_serializer = 'json'
+accept_content = ['json']
+timezone = 'Europe/Oslo'
+enable_utc = True
