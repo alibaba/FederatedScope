a9e06b22008f6639c38d02c8eb2b1c88270d5532
rayrayraykk <18007356109@163.com>
2022-07-19 16:13:00 +0800

    update ssh manager

--- federatedscope/organizer/client.py
+++ federatedscope/organizer/client.py
@@ -2,6 +2,8 @@ import cmd
 import time
 from celery import Celery
 
+from federatedscope.organizer.utils import SSHManager
+
 organizer = Celery()
 organizer.config_from_object('cfg_client')
 
@@ -14,63 +16,86 @@ class OrganizerClient(cmd.Cmd):
     timeout = 10
 
     def do_add_ecs(self, line):
-        'Add Ecs (ip, user, psw): add_ecs 172.X.X.X root 12345 port'
-        ip, user, psw, port = line.split(' ')
-        self.ecs[len(self.ecs)] = {
-            'ip': ip,
-            'user': user,
-            'psw': psw,
-            'port': port
-        }
-        print(f"{ip} added.")
-        # TODO: set up FS here?
+        'Add Ecs (ip, user, psw, port): add_ecs 172.X.X.X root 12345 50002'
+        try:
+            ip, user, psw, port = line.split(' ')
+            key = f"{ip}:{port}"
+            if key in self.ecs:
+                raise ValueError(f"ECS `{key}` already exists.")
+            self.ecs[key] = SSHManager(ip, user, psw, port)
+            print(f"{self.ecs[key]} added.")
+            # TODO: set up FS here?
+        except Exception as error:
+            print(f"Invalid: {error}")
+
+    def do_del_ecs(self, line):
+        'Delete Ecs (ip, port): del_ecs 172.X.X.X 50002'
+        try:
+            ip, port = line.split(' ')
+            key = f"{ip}:{port}"
+            print(f"Delete {key} {self.ecs[key]}.")
+            del self.ecs[key]
+        except Exception as error:
+            print(f"Invalid: {error}")
 
     def do_display_ecs(self, line):
         'Display all saved ECS: display_ecs'
-        info = ""
-        for key, value in self.ecs.items():
-            info += f"ecs_id: {key}, info: {value}\n"
-        print(info)
+        try:
+            info = ""
+            for key, value in self.ecs.items():
+                info += f"ecs: {key}, info: {value}\n"
+            print(info)
+        except Exception as error:
+            print(f"Invalid: {error}")
 
     def do_create_room(self, line):
         'Create FS room in server with specific command (command, psw):' \
             'create_room --cfg ../../federatedscope/example_configs' \
             '/distributed_femnist_server.yaml 12345'
-        global organizer
-        psw = line.split(' ')[-1]
-        command = line[:-len(psw) - 1]
-        result = organizer.send_task('server.create_room', [command, psw])
-        cnt = 0
-        while (not result.ready()) and cnt < self.timeout:
-            print('Waiting for response... (will re-try in 1s)')
-            time.sleep(1)
-            cnt += 1
-        print(result.get(timeout=1))
+        try:
+            global organizer
+            psw = line.split(' ')[-1]
+            command = line[:-len(psw) - 1]
+            result = organizer.send_task('server.create_room', [command, psw])
+            cnt = 0
+            while (not result.ready()) and cnt < self.timeout:
+                print('Waiting for response... (will re-try in 1s)')
+                time.sleep(1)
+                cnt += 1
+            print(result.get(timeout=1))
+        except Exception as error:
+            print(f"Invalid: {error}")
 
     def do_display_room(self, line):
         'Display all FS rooms: display_room'
-        global organizer
-        result = organizer.send_task('server.display_room')
-        cnt = 0
-        while (not result.ready()) and cnt < self.timeout:
-            print('Waiting for response... (will re-try in 1s)')
-            time.sleep(1)
-            cnt += 1
-        print(result.get(timeout=1))
+        try:
+            global organizer
+            result = organizer.send_task('server.display_room')
+            cnt = 0
+            while (not result.ready()) and cnt < self.timeout:
+                print('Waiting for response... (will re-try in 1s)')
+                time.sleep(1)
+                cnt += 1
+            print(result.get(timeout=1))
+        except Exception as error:
+            print(f"Invalid: {error}")
 
     def do_join_room(self, line):
         'Join specific FS room (room_id, psw): JOIN ROOM 0 12345'
-        global organizer
-        room_id, psw = line.split(' ')
-        result = organizer.send_task('server.join_room', [room_id, psw])
-        cnt = 0
-        while (not result.ready()) and cnt < self.timeout:
-            print('Waiting for response... (will re-try in 1s)')
-            time.sleep(1)
-            cnt += 1
-        print(result.get(timeout=1))
+        try:
+            global organizer
+            room_id, psw = line.split(' ')
+            result = organizer.send_task('server.join_room', [room_id, psw])
+            cnt = 0
+            while (not result.ready()) and cnt < self.timeout:
+                print('Waiting for response... (will re-try in 1s)')
+                time.sleep(1)
+                cnt += 1
+            print(result.get(timeout=1))
+        except Exception as error:
+            print(f"Invalid: {error}")
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         return True
 
 
--- federatedscope/organizer/utils.py
+++ federatedscope/organizer/utils.py
@@ -1,3 +1,40 @@
+import time
+import paramiko
+
+
+class SSHManager(object):
+    def __init__(self, ip, user, psw, port):
+        self.ssh_port = 22
+        self.ip = ip
+        self.user = user
+        self.psw = psw
+        self.port = port
+        self.ssh, self.trans = None, None
+
+    def _connect(self):
+        self.trans = paramiko.Transport((self.ip, self.ssh_port))
+        self.trans.connect(username=self.user, password=self.psw)
+        self.ssh = paramiko.SSHClient()
+        self.ssh._transport = self.trans
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    def disconnect(self):
+        self.trans.close()
+
+    def exec_cmd(self, command):
+        if self.ssh is None:
+            self._connect()
+        stdin, stdout, stderr = self.ssh.exec_command(command)
+        time.sleep(1)
+        output = []
+        for line in iter(stdout.readline, ""):
+            output.append(line)
+        return output
+
+    def setup_fs(self):
+        pass
+
+
 def anonymize(info, psw):
     for key, value in info.items():
         if isinstance(value, dict):
