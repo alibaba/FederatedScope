0198b47e6ccdeb8357b9c45a29d9bb9fcd40d586
rayrayraykk <18007356109@163.com>
2022-08-15 15:45:09 +0800

    add grid search to scripts

--- benchmark/FedHPOB/scripts/exp/run_mode.sh
+++ benchmark/FedHPOB/scripts/exp/run_mode.sh
@@ -15,6 +15,7 @@ for k in {1..5}; do
   python runner.py --cfg scripts/exp/${dataset}.yaml benchmark.device ${device} benchmark.model ${model} benchmark.type ${mode} benchmark.data ${dataset} benchmark.algo ${algo} optimizer.type bo_rf || echo "continue"
   python runner.py --cfg scripts/exp/${dataset}.yaml benchmark.device ${device} benchmark.model ${model} benchmark.type ${mode} benchmark.data ${dataset} benchmark.algo ${algo} optimizer.type bo_kde || echo "continue"
   python runner.py --cfg scripts/exp/${dataset}.yaml benchmark.device ${device} benchmark.model ${model} benchmark.type ${mode} benchmark.data ${dataset} benchmark.algo ${algo} optimizer.type de || echo "continue"
+  python runner.py --cfg scripts/exp/${dataset}.yaml benchmark.device ${device} benchmark.model ${model} benchmark.type ${mode} benchmark.data ${dataset} benchmark.algo ${algo} optimizer.type grid_search || echo "continue"
 
   python runner.py --cfg scripts/exp/${dataset}.yaml benchmark.device ${device} benchmark.model ${model} benchmark.type ${mode} benchmark.data ${dataset} benchmark.algo ${algo} optimizer.type hb || echo "continue"
   python runner.py --cfg scripts/exp/${dataset}.yaml benchmark.device ${device} benchmark.model ${model} benchmark.type ${mode} benchmark.data ${dataset} benchmark.algo ${algo} optimizer.type bohb || echo "continue"
