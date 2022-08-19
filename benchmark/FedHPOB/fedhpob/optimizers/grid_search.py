cd75b24a28c96189ed81658024bf5287f8f0329f
rayrayraykk <18007356109@163.com>
2022-08-19 15:23:41 +0800

    hardcode grid space

--- benchmark/FedHPOB/fedhpob/optimizers/grid_search.py
+++ benchmark/FedHPOB/fedhpob/optimizers/grid_search.py
@@ -3,6 +3,7 @@
 import time
 import random
 import logging
+import ConfigSpace as CS
 from ConfigSpace.util import generate_grid
 
 from fedhpob.config import fhb_cfg
@@ -19,13 +20,27 @@ def run_grid_search(cfg):
         cfg.benchmark.data,
         cfg.benchmark.algo,
         device=cfg.benchmark.device)
-    configspace = cfg.benchmark.configuration_space[0]
-    grid = generate_grid(configspace)
+    # configspace = cfg.benchmark.configuration_space[0]
+    # This is only for FEMNIST grid search exp
+    new_configspace = CS.ConfigurationSpace()
+    new_configspace.add_hyperparameter(
+        CS.CategoricalHyperparameter('batch', choices=[16, 64]))
+    new_configspace.add_hyperparameter(
+        CS.CategoricalHyperparameter('dropout', choices=[0.0, 0.5]))
+    new_configspace.add_hyperparameter(
+        CS.CategoricalHyperparameter(
+            'lr', choices=[0.01, 0.02783, 0.07743, 0.21544, 0.59948, 1.0]))
+    new_configspace.add_hyperparameter(
+        CS.CategoricalHyperparameter('step', choices=[2, 4]))
+    new_configspace.add_hyperparameter(
+        CS.CategoricalHyperparameter('wd', choices=[0.0, 0.1]))
+    grid = generate_grid(new_configspace)
+    print(len(grid))
     budget = cfg.optimizer.max_budget
     history = []
     # TODO: Add a gap here
-    for idx in range(len(grid)):
-        config = grid[idx]
+    for idx in range(999999999):
+        config = grid[idx % len(grid)]
         main_fidelity = {
             'round': int(budget),
             'sample_client': cfg.benchmark.sample_client
