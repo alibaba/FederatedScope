56553e0cf27d70d4950419c94cac6b885ebcf524
rayrayraykk <18007356109@163.com>
2022-09-01 17:23:30 +0800

    add process

--- federatedscope/core/translator/data_translator.py
+++ federatedscope/core/translator/data_translator.py
@@ -3,12 +3,15 @@ from federatedscope.core.auxiliaries.transform_builder import get_transform
 from federatedscope.core.interface.base_data import ClientData, \
     StandaloneDataDict
 
+# TODO: Implement CVDataTranslator, NLPDataTranslator from `databuilder`
+
 
 class DataTranslator(StandaloneDataDict):
     def __init__(self, dataset, global_cfg, package=None):
         self.splitter = get_splitter(global_cfg)
         if package is not None:
             self.transform_funcs = get_transform(global_cfg, package)
+        self.process()
         super(DataTranslator, self).__init__(dataset, global_cfg)
 
     @staticmethod
