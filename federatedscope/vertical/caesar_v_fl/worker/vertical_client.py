right ss but wrong result

federatedscope/vertical/caesar_v_fl/worker/vertical_client.py
@@ -24,6 +24,8 @@ class vFLClient(Client):
     Marries Secret Sharing: Secure Large-Scale Sparse Logistic Regression and
     Applications in Risk Control` [Chen, et al., 2021]
     (https://arxiv.org/abs/2008.08753)
+
+    For convenience, we assume that client B has the label.
     """
     def __init__(self,
                  ID=-1,
@@ -50,7 +52,6 @@ class vFLClient(Client):
         # w_a = <w_a>_1 + <w_a>_2 and w_b = <w_b>_1 + <w_b>_2
         self.my_part_of_others_para = None
         self.my_part_of_my_para = None
-        self.others_part_of_my_para = None
         self.others_public_key = None
         self.my_part_of_my_z = None
         self.my_part_of_others_part_of_my_z = None
@@ -95,8 +96,10 @@ class vFLClient(Client):
 
     def callback_func_for_model_para(self, message: Message):
         self.total_round_num, self.my_para = message.content
-        self.my_part_of_my_para, self.others_part_of_my_para = \
+        self.my_part_of_my_para, others_part_of_my_para = \
             self.ss.secret_split(self.my_para)
+        # print(self.ID, self.my_para, self.my_part_of_my_para,
+        # others_part_of_my_para)
         self.comm_manager.send(
             Message(msg_type='public_key_and_para',
                     sender=self.ID,
@@ -105,21 +108,22 @@ class vFLClient(Client):
                         if each != self.server_id
                     ],
                     state=self.state,
-                    content=(self.my_public_key, self.others_part_of_my_para)))
+                    content=(self.my_public_key, others_part_of_my_para)))
 
     def callback_func_for_public_key_and_para(self, message: Message):
         self.others_public_key, self.my_part_of_others_para = message.content
         if self.own_label:
-            self.move_to_the_next_train(None)
+            self.move_to_the_next_train()
 
     # start training
-    # B sample data
-    # B call encode()
+    # B samples data
+    # B calls encode()
 
-    def move_to_the_next_train(self, message: Message):
+    def move_to_the_next_train(self):
         index, self.input_x, input_y = self.sample_data()
         self.y = input_y
         self.batch_index = index
+        print(self.ID, self.input_x, self.y)
         self.comm_manager.send(
             Message(msg_type='sample_data',
                     sender=self.ID,
@@ -131,18 +135,21 @@ class vFLClient(Client):
                     content=self.batch_index))
         self.encode()
 
-    # A sample the same data as B
-    # A also call encode()
+    # A samples the same data as B
+    # A also calls encode()
 
     def callback_func_for_sample_data(self, message: Message):
         index = message.content
         self.batch_index = index
         self.input_x = self.sample_data(index=self.batch_index)
+        print(self.ID, self.input_x)
         self.encode()
 
     # both clients do the following
     def encode(self):
-        tmp = [
+        print(self.ID, self.my_para, self.my_part_of_my_para,
+              self.my_part_of_others_para)
+        en_para = [
             self.my_public_key.encrypt(x) for x in self.my_part_of_others_para
         ]
         self.comm_manager.send(
@@ -153,15 +160,48 @@ class vFLClient(Client):
                         if each != self.server_id
                     ],
                     state=self.state,
-                    content=tmp))
+                    content=en_para))
+
+    def mod_matmul(self, data, para, epsilon, mod_number):
+        res = [0 for _ in range(data.shape[0])]
+        for i in range(data.shape[0]):
+            tmp = 0
+            for j in range(data.shape[1]):
+                a = data[i, j].item()
+                b = para[j].item()
+                tmp += a * b  # % mod_number
+                tmp = tmp  # % mod_number
+            res[i] = round(tmp / epsilon)
+            res[i] = res[i] % mod_number
+        return res
 
     def callback_func_for_encrypted(self, message: Message):
-        tmp = message.content
-        self.my_part_of_my_z = np.matmul(self.input_x, self.my_part_of_my_para)
+        en_para = message.content
+        # self.my_part_of_my_z =
+        #       np.matmul(self.input_x, self.my_part_of_my_para)
+        upgrade_x = self.ss.float2fixedpoint(self.input_x)
 
-        tmp1 = np.matmul(self.input_x, tmp)
-        self.my_part_of_others_part_of_my_z, tmp = \
+        self.my_part_of_my_z = self.mod_matmul(upgrade_x,
+                                               self.my_part_of_my_para,
+                                               self.ss.epsilon,
+                                               self.ss.mod_number)
+        # self.my_part_of_my_z = self.ss.downgrade(self.my_part_of_my_z)
+
+        print(self.ID, self.my_part_of_my_z)
+
+        # tmp1 = np.matmul(self.input_x, en_para)
+        tmp1 = self.mod_matmul(upgrade_x, en_para, self.ss.epsilon,
+                               self.ss.mod_number)
+        # tmp1 = self.ss.downgrade(tmp1)
+
+        print(self.ID, "x * others w:", tmp1)
+
+        self.my_part_of_others_part_of_my_z, en_z = \
             self.ss.secret_split_for_piece_of_ss(tmp1)
+
+        print(self.my_part_of_others_part_of_my_z)
+        print(en_z)
+        print("====")
 
         self.comm_manager.send(
             Message(msg_type='decrypt',
@@ -171,12 +211,12 @@ class vFLClient(Client):
                         if each != self.server_id
                     ],
                     state=self.state,
-                    content=tmp))
+                    content=en_z))
 
     def callback_func_for_decrypt(self, message: Message):
-        tmp1 = message.content
+        en_z = message.content
         self.my_part_of_my_part_of_others_z = [
-            self.my_private_key.decrypt(x) for x in tmp1
+            self.my_private_key.decrypt(x) for x in en_z
         ]
         if not self.own_label:
             self.a_computes()
@@ -186,13 +226,26 @@ class vFLClient(Client):
     # A encrypts the above three vectors and sends them to B
 
     def a_computes(self):
+
         tmp1 = [
             self.my_part_of_my_z[i] + self.my_part_of_others_part_of_my_z[i] +
             self.my_part_of_my_part_of_others_z[i]
             for i in range(len(self.my_part_of_my_z))
         ]
-        tmp2 = [x**2 for x in tmp1]
-        tmp3 = [x**3 for x in tmp1]
+
+        # print(tmp1)
+        tmp1 = self.ss.mod_funs(tmp1)
+        print("z1: ", tmp1)
+
+        tmp2 = [x.item()**2 for x in tmp1]
+        tmp2 = self.ss.downgrade(tmp2)
+        # print(tmp1)
+        # print(tmp2)
+        tmp3 = [x.item()**3 % self.ss.mod_number for x in tmp1]
+        tmp3 = self.ss.downgrade(tmp3)
+        tmp3 = self.ss.downgrade(tmp3)
+        # print(tmp3)
+
         tmp1 = [self.my_public_key.encrypt(x) for x in tmp1]
         tmp2 = [self.my_public_key.encrypt(x) for x in tmp2]
         tmp3 = [self.my_public_key.encrypt(x) for x in tmp3]
@@ -222,6 +275,30 @@ class vFLClient(Client):
     # B keeps <g_b>_2
     # B sends [<g_b>_1]_a to A
     # B sends [<e>_2]_b to A
+    '''
+    def biginteger_mod(self, n, m):
+        ans = 0
+        while n < 0:
+            n += m
+        n = int(n)
+        s = str(n)
+        for i in range(len(s)):
+            ans = ((ans * 10 + int(s[i])) % m)
+        return ans
+    '''
+
+    def err_mul_data(self, e, data, epsilon, mod_number):
+        res = [0 for _ in range(data.shape[1])]
+        for j in range(data.shape[1]):
+            tmp = 0
+            for i in range(data.shape[0]):
+                a = e[i].item()
+                b = data[i, j].item()
+                tmp += a * b  # % mod_number
+                tmp = tmp  # % mod_number
+            res[j] = round(tmp / epsilon)
+            res[j] = res[j] % mod_number
+        return res
 
     def callback_func_for_three_values(self, message: Message):
         tmp1, tmp2, tmp3 = message.content
@@ -230,7 +307,17 @@ class vFLClient(Client):
             self.my_part_of_others_part_of_my_z[i]
             for i in range(len(self.my_part_of_my_z))
         ]
+
+        # print(z2)
+        z2 = self.ss.mod_funs(z2)
+        print("z2: ", z2)
+
         za = [tmp1[i] + z2[i] for i in range(len(tmp1))]
+        # print(za)
+        za = self.ss.mod_funs(za)
+        print("za: ", za)
+        print("za float: ", self.ss.fixedpoint2float(za))
+
         # z3a = [
         #    tmp3[i] + 3 * tmp2[i] * z2[i] + 3 * tmp1[i] * z2[i] * z2[i] +
         #    z2[i] * z2[i] * z2[i] for i in range(len(tmp3))]
@@ -238,21 +325,81 @@ class vFLClient(Client):
         # Here is the approximation of sigmoid function,
         # different data and different forms may lead to diversity,
         # here linear is enough
-        y_hat_a = [
-            1 / 2 + 1 / 4 * za[i]  # - 1 / 48 * z3a[i]
-            for i in range(len(za))
-        ]
 
-        e_a = y_hat_a - self.y
+        # print(za)
+        t1 = [self.ss.float2fixedpoint(1 / 4) * x for x in za]
+        # print(t1)
+        t1 = self.ss.downgrade(t1)
+        # print(t1)
+
+        y_hat_a = [self.ss.float2fixedpoint(1 / 2) + x for x in t1]
+        y_hat_a = self.ss.mod_funs(y_hat_a)
+        # y_hat_a = [x//self.ss.epsilon**2 for x in y_hat_a]
+        print("y_hat_a: ", y_hat_a)
+        """
+        zzz = [3 * tmp2[i].item() * z2[i].item() % self.ss.mod_number
+               + 3 * tmp1[i].item() * z2[i].item() **2 % self.ss.mod_number
+               + z2[i].item() ** 3  % self.ss.mod_number
+                    for i in range(len(tmp3))]
+        zzz = self.ss.mod_funs(zzz)
+        print(zzz)
+        ttt = [self.ss.float2fixedpoint(1/48) * zzz[i]
+                for i in range(len(zzz))]
+        ttt = self.ss.downgrade(ttt)
+        y_hat_a = [y_hat_a[i] - ttt[i] for i in range(len(y_hat_a))]
+        y_hat_a = self.ss.mod_funs(y_hat_a)
+        """
+
+        # y_hat_a = [
+        #    1 / 2 + 1 / 4 * za[i]  # - 1 / 48 * z3a[i]
+        #    for i in range(len(za))
+        # ]
+
+        upgrade_y = self.ss.float2fixedpoint(self.y)
+        # e_a = y_hat_a - self.y
+        # print(y_hat_a)
+        # print(upgrade_y)
+        e_a = self.ss.mod_funs(y_hat_a - upgrade_y)
+        print("e_a: ", e_a)
+
         y_hat_2, y_hat_1 = self.ss.secret_split_for_piece_of_ss(y_hat_a)
-        e_2 = y_hat_2 - self.y
-        g_b_a = np.matmul(e_a, self.input_x)
+        print("y_hat_a: ", y_hat_a)
+        print("y_hat_1: ", y_hat_1)
+        print("y_hat_2: ", y_hat_2)
+
+        # e_2 = y_hat_2 - self.y
+        e_2 = self.ss.mod_funs(y_hat_2 - upgrade_y)
+        print("e_2: ", e_2)
+
+        upgrade_x = self.ss.float2fixedpoint(self.input_x)
+        # g_b_a = np.matmul(e_a, self.input_x)
+
+        # print(e_a)
+        # print(upgrade_x)
+        g_b_a = self.err_mul_data(e_a, upgrade_x, self.ss.epsilon,
+                                  self.ss.mod_number)
+        # print(g_b_a)
+        # g_b_a = self.ss.downgrade(g_b_a)
+        print("g_b_a: ", g_b_a)
+
         g_b_2, g_b_1 = self.ss.secret_split_for_piece_of_ss(g_b_a)
+        print("g_b_1: ", g_b_1)
+        print("g_b_2: ", g_b_2)
+
         # user b update w_b_2
+        upgrade_lr = self.ss.float2fixedpoint(self.lr)
+        t1 = [upgrade_lr * x for x in g_b_2]
+        # t1 = self.ss.downgrade(t1)
+        self.my_part_of_my_para = self.ss.mod_funs(
+            [self.my_part_of_my_para[i] - t1[i] for i in range(len(t1))])
+        print("lr * g_b_2", t1)
+        print("new w_b_2", self.my_part_of_my_para)
+        '''
         self.my_part_of_my_para = [
             self.my_part_of_my_para[i] - self.lr * g_b_2[i]
             for i in range(len(g_b_2))
         ]
+        '''
         encrypted_e_2 = [self.my_public_key.encrypt(x) for x in e_2]
         self.comm_manager.send(
             Message(msg_type="complicated_comp",
@@ -278,20 +425,61 @@ class vFLClient(Client):
         y_hat_1, en_g_b_1, encrypted_e_2 = message.content
         g_b_1 = [self.my_private_key.decrypt(x) for x in en_g_b_1]
         e_1 = [self.my_private_key.decrypt(x) for x in y_hat_1]
+        print("e_1: ", e_1)
 
-        g_a_1 = np.matmul(e_1, self.input_x)
-        g_a_2 = np.matmul(encrypted_e_2, self.input_x)
+        # g_a_1 = np.matmul(e_1, self.input_x)
+        upgrade_x = self.ss.float2fixedpoint(self.input_x)
+        g_a_1 = self.err_mul_data(e_1, upgrade_x, self.ss.epsilon,
+                                  self.ss.mod_number)
+        # g_a_1 = self.ss.downgrade(g_a_1)
+        print("g_a_1: ", g_a_1)
+
+        # g_a_2 = np.matmul(encrypted_e_2, self.input_x)
+        g_a_2 = self.err_mul_data(encrypted_e_2, upgrade_x, self.ss.epsilon,
+                                  self.ss.mod_number)
+        # g_a_2 = self.ss.downgrade(g_a_2)
+        print("g_a_2: ", g_a_2)
+
+        print("g_a: ",
+              self.ss.mod_funs([x + y for (x, y) in zip(g_a_1, g_a_2)]))
+
+        # print(e_1)
+        # print(self.ss.secret_reconstruct((g_a_1, g_a_2)))
+
         g_a_2_1, g_a_2_2 = self.ss.secret_split_for_piece_of_ss(g_a_2)
+        print("g_a_2_1: ", g_a_2_1)
+        print("g_a_2_2: ", g_a_2_2)
         # user A updates w_a_1
+        upgrade_lr = self.ss.float2fixedpoint(self.lr)
+        t1 = self.ss.mod_funs(
+            [g_a_1[i] + g_a_2_1[i] for i in range(len(g_a_1))])
+        t2 = [upgrade_lr * x for x in t1]
+        # t2 = self.ss.downgrade(t2)
+        self.my_part_of_my_para = self.ss.mod_funs(
+            [self.my_part_of_my_para[i] - t2[i] for i in range(len(t2))])
+
+        print("lr * (g_a_1 + g_a_2_1): ", t2)
+        print("new w_a_1: ", self.my_part_of_my_para)
+        '''
         self.my_part_of_my_para = [
             self.my_part_of_my_para[i] - self.lr * (g_a_1[i] + g_a_2_1[i])
             for i in range(len(g_a_1))
         ]
+        '''
         # user A updates w_b_1
+        t3 = [upgrade_lr * x for x in g_b_1]
+        # t3 = self.ss.downgrade(t3)
+        self.my_part_of_others_para = self.ss.mod_funs(
+            [self.my_part_of_others_para[i] - t3[i] for i in range(len(t3))])
+
+        print("lr * g_b_1: ", t3)
+        print("new w_b_1: ", self.my_part_of_others_para)
+        '''
         self.my_part_of_others_para = [
             self.my_part_of_others_para[i] - self.lr * g_b_1[i]
             for i in range(len(g_b_1))
         ]
+        '''
         self.comm_manager.send(
             Message(msg_type="para_update",
                     sender=self.ID,
@@ -310,24 +498,36 @@ class vFLClient(Client):
     # B sends <w_a>_2 to A
 
     def callback_func_for_b_to_update_para(self, message: Message):
-        tmp = message.content
-        tmp = [self.my_private_key.decrypt(x) for x in tmp]
+        g_a_2_2 = message.content
+        g_a_2_2 = [self.my_private_key.decrypt(x) for x in g_a_2_2]
+
         #  user B updates w_a_2
+        upgrade_lr = self.ss.float2fixedpoint(self.lr)
+        t1 = [upgrade_lr * x for x in g_a_2_2]
+        # t1 = self.ss.downgrade(t1)
+
+        self.my_part_of_others_para = self.ss.mod_funs(
+            [self.my_part_of_others_para[i] - t1[i] for i in range(len(t1))])
+
+        print("lr * g_a_2_2: ", t1)
+        print("new w_a_2: ", self.my_part_of_others_para)
+        '''
         self.my_part_of_others_para = [
             self.my_part_of_others_para[i] - self.lr * tmp[i]
             for i in range(len(tmp))
         ]
+        '''
         self.state += 1
         if self.state < self.total_round_num:
             # Move to next round of training
             logger.info(f'----------- Starting a new training round (Round '
                         f'#{self.state}) -------------')
             # self.final_step() 可用来查看每轮的prediction
-            self.move_to_the_next_train(message=None)
+            self.move_to_the_next_train()
         else:
             self.final_step()
 
-    # B sends <w_a>_2 to A
+    # call exchange_paras(): two clients send each other their part of paras
 
     def final_step(self):
         self.comm_manager.send(
@@ -359,6 +559,10 @@ class vFLClient(Client):
         para = message.content
         self.my_para = self.ss.secret_reconstruct(
             (self.my_part_of_my_para, para))
+        # print(para)
+        # self.my_para = self.my_para / self.ss.epsilon
+
+        print("my_para", self.my_para)
         self.comm_manager.send(
             Message(msg_type='para_for_server',
                     sender=self.ID,

federatedscope/vertical/simple_secret_sharing/secret_sharing.py
@@ -24,17 +24,18 @@ class AdditiveSecretSharing(SecretSharing):
     AdditiveSecretSharing class, which can split a number into frames and
     recover it by summing up
     """
-    def __init__(self, shared_party_num, size=20):
+    def __init__(self, shared_party_num, size=10):
         super(SecretSharing, self).__init__()
         assert shared_party_num > 1, "AdditiveSecretSharing require " \
                                      "shared_party_num > 1"
         self.shared_party_num = shared_party_num
         self.maximum = 2**size
-        self.mod_number = 2 * self.maximum + 1
-        self.epsilon = 1e4
+        self.mod_number = 2**(2 * size)
+        self.epsilon = 1e2
         self.mod_funs = np.vectorize(lambda x: x % self.mod_number)
         self.float2fixedpoint = np.vectorize(self._float2fixedpoint)
         self.fixedpoint2float = np.vectorize(self._fixedpoint2float)
+        self.downgrade = np.vectorize(self._downgrade)
 
     def secret_split(self, secret):
         """
@@ -57,15 +58,13 @@ class AdditiveSecretSharing(SecretSharing):
             shape = [self.shared_party_num - 1]
 
         secret = self.float2fixedpoint(secret)
-        secret_seq = np.random.uniform(low=0, high=self.mod_number, size=shape)
-        # last_seq = self.mod_funs(secret - self.mod_funs(np.sum(secret_seq,
-        # axis=0)))
-        last_seq = secret - np.sum(secret_seq, axis=0)
+        secret_seq = np.random.randint(low=0, high=self.mod_number, size=shape)
+        last_seq = self.mod_funs(secret -
+                                 self.mod_funs(np.sum(secret_seq, axis=0)))
 
         secret_seq = np.append(secret_seq,
                                np.expand_dims(last_seq, axis=0),
                                axis=0)
-
         return secret_seq
 
     def secret_split_for_piece_of_ss(self, secret):
@@ -87,15 +86,13 @@ class AdditiveSecretSharing(SecretSharing):
             shape = [self.shared_party_num - 1] + list(secret.shape)
         else:
             shape = [self.shared_party_num - 1]
-        secret_seq = np.random.uniform(low=0, high=self.mod_number, size=shape)
-        # last_seq = self.mod_funs(secret - self.mod_funs(np.sum(secret_seq,
-        # axis=0)))
-        last_seq = secret - np.sum(secret_seq, axis=0)
+        secret_seq = np.random.randint(low=0, high=self.mod_number, size=shape)
+        last_seq = self.mod_funs(secret -
+                                 self.mod_funs(np.sum(secret_seq, axis=0)))
 
         secret_seq = np.append(secret_seq,
                                np.expand_dims(last_seq, axis=0),
                                axis=0)
-        # secret_seq = [self.fixedpoint2float(x) for x in secret_seq]
         return secret_seq
 
     def secret_reconstruct(self, secret_seq):
@@ -123,13 +120,19 @@ class AdditiveSecretSharing(SecretSharing):
         return merge_model
 
     def _float2fixedpoint(self, x):
-        x = round(x * self.epsilon, 0)
+        x = round(x * self.epsilon)
         assert abs(x) < self.maximum
-        return x % self.mod_number
+        return x  # % self.mod_number
 
     def _fixedpoint2float(self, x):
         x = x % self.mod_number
+        # assert x <= self.maximum or x >= self.mod_number - self.maximum
         if x > self.maximum:
             return -1 * (self.mod_number - x) / self.epsilon
         else:
             return x / self.epsilon
+
+    def _downgrade(self, x):
+        x = round(x / self.epsilon)
+        x = x % self.mod_number
+        return x