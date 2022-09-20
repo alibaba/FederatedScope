8eee1c7adc1d8a2f9ef31c8d542624b0786d4b0e
rayrayraykk <18007356109@163.com>
2022-09-20 19:48:38 +0800

    add centralized torch trainer

---
+++ federatedscope/contrib/trainer/torch_example.py
@@ -0,0 +1,97 @@
+import inspect
+from federatedscope.register import register_trainer
+
+# An example for converting torch training process to FS training process
+# Try with FEMNIST:
+#  python federatedscope/main.py --cfg scripts/example_configs/femnist.yaml \
+#  trainer.type mytorchtrainer federate.sample_client_rate 0.01 \
+#  federate.total_round_num 5 eval.best_res_update_round_wise_key test_loss
+
+
+class MyTorchTrainer(object):
+    def __init__(self, model, data, device, **kwargs):
+        import torch
+        # NN modules
+        self.model = model
+        # FS `ClientData` or your own data
+        self.data = data
+        # Device name
+        self.device = device
+        # kwargs
+        self.kwargs = kwargs
+        # Criterion & Optimizer
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.SGD(self.model.parameters(),
+                                         lr=0.001,
+                                         momentum=0.9,
+                                         weight_decay=1e-4)
+
+    def train(self):
+        # _hook_on_fit_start_init
+        self.model.to(self.device)
+        self.model.train()
+        self.optimizer.zero_grad()
+
+        total_loss = num_samples = 0
+        # _hook_on_batch_start_init
+        for x, y in self.data['train']:
+            # _hook_on_batch_forward
+            x, y = x.to(self.device), y.to(self.device)
+            outputs = self.model(x)
+            loss = self.criterion(outputs, y)
+
+            # _hook_on_batch_backward
+            loss.backward()
+            self.optimizer.step()
+
+            # _hook_on_batch_end
+            total_loss += loss.item() * y.shape[0]
+            num_samples += y.shape[0]
+
+        # _hook_on_fit_end
+        return num_samples, self.model.cpu().state_dict(), \
+            {'loss_total': total_loss}
+
+    def evaluate(self, target_data_split_name='test'):
+        import torch
+        with torch.no_grad():
+            self.model.to(self.device)
+            self.model.eval()
+            total_loss = num_samples = 0
+            # _hook_on_batch_start_init
+            for x, y in self.data[target_data_split_name]:
+                # _hook_on_batch_forward
+                x, y = x.to(self.device), y.to(self.device)
+                pred = self.model(x)
+                loss = self.criterion(pred, y)
+
+                # _hook_on_batch_end
+                total_loss += loss.item() * y.shape[0]
+                num_samples += y.shape[0]
+
+            # _hook_on_fit_end
+            return {
+                f'{target_data_split_name}_loss': total_loss,
+                f'{target_data_split_name}_total': num_samples
+            }
+
+    def update(self, model_parameters, strict=False):
+        self.model.load_state_dict(model_parameters, strict)
+
+    def get_model_para(self):
+        return self.model.cpu().state_dict()
+
+    def print_trainer_meta_info(self):
+        sign = inspect.signature(self.__init__).parameters.values()
+        meta_info = tuple([(val.name, getattr(self, val.name))
+                           for val in sign])
+        return f'{self.__class__.__name__}{meta_info}'
+
+
+def call_my_torch_trainer(trainer_type):
+    if trainer_type == 'mytorchtrainer':
+        trainer_builder = MyTorchTrainer
+        return trainer_builder
+
+
+register_trainer('mytorchtrainer', call_my_torch_trainer)
--- federatedscope/core/workers/client.py
+++ federatedscope/core/workers/client.py
@@ -410,11 +410,10 @@ class Client(Worker):
                                  self._cfg.dataloader.batch_size
                 else:
                     num_sample = self._cfg.train.local_update_steps * \
-                                 self.trainer.ctx.num_train_batch
+                                 len(self.data['train'])
                 join_in_info['num_sample'] = num_sample
                 if self._cfg.trainer.type == 'nodefullbatch_trainer':
-                    join_in_info['num_sample'] = \
-                        self.trainer.ctx.data['data'].x.shape[0]
+                    join_in_info['num_sample'] = self.data['data'].x.shape[0]
             elif requirement.lower() == 'client_resource':
                 assert self.comm_bandwidth is not None and self.comp_speed \
                        is not None, "The requirement join_in_info " \
