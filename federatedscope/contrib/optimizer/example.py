2389bd1e0b39b2cc87069b0de2d055873778e568
gaodawei.gdw <gaodawei.gdw@alibaba-inc.com>
2022-09-21 21:34:33 +0800

    bug fix

--- federatedscope/contrib/optimizer/example.py
+++ federatedscope/contrib/optimizer/example.py
@@ -1,16 +1,16 @@
 from federatedscope.register import register_optimizer
 
 
-def call_my_optimizer(model, type, lr, **kwargs):
+def call_my_optimizer(type):
     try:
         import torch.optim as optim
     except ImportError:
         optim = None
         optimizer = None
 
-    if type == 'myoptimizer':
+    if type.lower() == 'myoptimizer':
         if optim is not None:
-            optimizer = optim.Adam(model.parameters(), lr=lr, **kwargs)
+            optimizer = optim.Adam
         return optimizer
 
 
