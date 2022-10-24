0a395c5c96ee644dad34a4f25bdcc29c80cd81ba
xkxxfyf <2014201870@ruc.edu.cn>
2022-10-25 03:45:00 +0800

    Update utils.py

--- federatedscope/core/data/utils.py
+++ federatedscope/core/data/utils.py
@@ -38,6 +38,8 @@ def load_dataset(config):
         dataset, modified_config = load_quadratic_dataset(config)
     elif config.data.type.lower() in ['femnist', 'celeba']:
         from federatedscope.cv.dataloader import load_cv_dataset
+    elif config.data.type.lower() in ['cifar4cl', 'cifar4lp']:
+        from federatedscope.cl.dataloader import load_cifar_dataset
         dataset, modified_config = load_cv_dataset(config)
     elif config.data.type.lower() in [
             'shakespeare', 'twitter', 'subreddit', 'synthetic'
