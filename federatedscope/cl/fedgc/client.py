d213297e7e4c48ff0bcb82a9cca6f32aa9c20ab8
xkxxfyf <2014201870@ruc.edu.cn>
2022-10-25 22:10:20 +0800

    modify yapf format

--- federatedscope/cl/fedgc/client.py
+++ federatedscope/cl/fedgc/client.py
@@ -9,6 +9,7 @@ from federatedscope.core.auxiliaries.utils import merge_dict
 
 logger = logging.getLogger(__name__)
 
+
 class GlobalContrastFLClient(Client):
     r"""
     GlobalContrastFL(Fedgc) Client receive aggregated model weight from
@@ -23,7 +24,8 @@ class GlobalContrastFLClient(Client):
         self.register_handlers('address', self.callback_funcs_for_address)
         self.register_handlers('model_para',
                                self.callback_funcs_for_pred_embedding)
-        self.register_handlers('global_loss', self.callback_funcs_for_local_backward)
+        self.register_handlers('global_loss',
+                               self.callback_funcs_for_local_backward)
         self.register_handlers('ss_model_para',
                                self.callback_funcs_for_model_para)
 
@@ -37,7 +39,7 @@ class GlobalContrastFLClient(Client):
         model_para = self.trainer.train_with_global_loss(global_loss)
         self.trainer.update(model_para)
         self.state = round
-        sample_size= self.trainer.num_samples
+        sample_size = self.trainer.num_samples
         model_para = self.trainer.get_model_para()
 
         self.comm_manager.send(
@@ -46,19 +48,19 @@ class GlobalContrastFLClient(Client):
                     receiver=[sender],
                     state=self.state,
                     content=(sample_size, model_para)))
-        
+
     def callback_funcs_for_pred_embedding(self, message: Message):
         round, sender, content = message.state, message.sender, message.content
         self.trainer.update(content)
         sample_size, model_para, results = self.trainer.train()
         self.state = round
         pred_embedding = self.trainer.get_train_pred_embedding()
-        
-        train_log_res = self._monitor.format_eval_res(
-            results,
-            rnd=self.state,
-            role='Client #{}'.format(self.ID),
-            return_raw=True)
+
+        train_log_res = self._monitor.format_eval_res(results,
+                                                      rnd=self.state,
+                                                      role='Client #{}'.format(
+                                                          self.ID),
+                                                      return_raw=True)
         logger.info(train_log_res)
 
         self.comm_manager.send(
@@ -67,4 +69,3 @@ class GlobalContrastFLClient(Client):
                     receiver=[sender],
                     state=self.state,
                     content=(pred_embedding)))
-
