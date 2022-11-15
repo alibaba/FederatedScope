de49bb1a72a7f356ce6ff6c089c0399617c4bc26
qbc <qianbingchen.qbc@alibaba-inc.com>
2022-11-15 22:13:51 +0800

    update text_xgb.py

--- tests/test_xgb.py
+++ tests/test_xgb.py
@@ -30,6 +30,7 @@ class XGBTest(unittest.TestCase):
         cfg.train.optimizer.num_of_trees = 4
         cfg.train.optimizer.max_tree_depth = 3
 
+        cfg.data.root = 'test_data/'
         cfg.data.type = 'adult'
         cfg.data.size = 2000
 
