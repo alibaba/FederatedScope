153d363691ec4dc653c5b4c53ba27d00576d2dec
Weirui Kuang <39145382+rayrayraykk@users.noreply.github.com>
2022-11-24 20:42:04 +0800

    Add checks for completeness of msg_handler (#388)

--- federatedscope/core/workers/base_server.py
+++ federatedscope/core/workers/base_server.py
@@ -5,11 +5,10 @@ from federatedscope.core.workers import Worker
 class BaseServer(Worker):
     def __init__(self, ID, state, config, model, strategy):
         super(BaseServer, self).__init__(ID, state, config, model, strategy)
-        # TODO: move to worker
         self.msg_handlers = dict()
+        self.msg_handlers_str = dict()
 
-    # TODO: move to worker
-    def register_handlers(self, msg_type, callback_func):
+    def register_handlers(self, msg_type, callback_func, send_msg=[None]):
         """
         To bind a message type with a handling function.
 
@@ -19,6 +18,7 @@ class BaseServer(Worker):
                 message
         """
         self.msg_handlers[msg_type] = callback_func
+        self.msg_handlers_str[msg_type] = (callback_func.__name__, send_msg)
 
     def _register_default_handlers(self):
         """
@@ -38,10 +38,15 @@ class BaseServer(Worker):
             ``metrics``                  ``callback_funcs_for_metrics``
             ============================ ==================================
         """
-        self.register_handlers('join_in', self.callback_funcs_for_join_in)
-        self.register_handlers('join_in_info', self.callback_funcs_for_join_in)
-        self.register_handlers('model_para', self.callback_funcs_model_para)
-        self.register_handlers('metrics', self.callback_funcs_for_metrics)
+        self.register_handlers('join_in', self.callback_funcs_for_join_in, [
+            'assign_client_id', 'ask_for_join_in_info', 'address', 'model_para'
+        ])
+        self.register_handlers('join_in_info', self.callback_funcs_for_join_in,
+                               ['address', 'model_para'])
+        self.register_handlers('model_para', self.callback_funcs_model_para,
+                               ['model_para', 'evaluate', 'finish'])
+        self.register_handlers('metrics', self.callback_funcs_for_metrics,
+                               ['converged'])
 
     @abc.abstractmethod
     def run(self):
