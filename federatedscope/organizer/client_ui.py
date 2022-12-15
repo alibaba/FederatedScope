7665e3400eba2157408a98c11882ee4f4908f399
rayrayraykk <18007356109@163.com>
2022-12-15 11:20:02 +0800

    update interface

--- federatedscope/organizer/client_ui.py
+++ federatedscope/organizer/client_ui.py
@@ -1,200 +1,242 @@
-"""
-To conduct this on your mac, please execute:
-```python
-conda create -n fs_client python=3.10
-conda activate fs_client
-pip install -e .
-conda install -c anaconda python.app
-pythonw client_ui.py
-```
-"""
+import PySimpleGUI as sg
 
-# TODO: delete this, cannot
 import time
-import datetime
 
 from celery import Celery
-from gooey import Gooey, GooeyParser
 
 from federatedscope.core.configs.config import CN
 from federatedscope.organizer.cfg_client import server_ip
 from federatedscope.organizer.utils import SSHManager, config2cmdargs, \
-    flatten_dict
+    flatten_dict, format_print
+
+TIMEOUT = 10
 
 
-def format_print(s):
-    print(f"[{str(datetime.datetime.now()).split('.')[0]}] - {s}")
+class UIEventHandler:
+    def __init__(self):
+        self.ecs_dict = dict()
+        self.task_dict = dict()
+        organizer = Celery()
+        organizer.config_from_object('cfg_client')
+
+    def handle_display_ecs(self, value):
+        info = ""
+        for k, v in self.ecs_dict.items():
+            info += f"ecs: {k}, info: {v}\n"
+        if info:
+            format_print(info)
+        else:
+            format_print('No saved ECS, please add ECS via `AddECS` '
+                         'first!')
+
+    def handle_add_ecs(self, value):
+        ip, user, psw = value['ip'], value['user'], value['password']
+        key = f"{ip}"
+        if key in self.ecs_dict:
+            raise ValueError(f"ECS `{key}` already exists.")
+        self.ecs_dict[key] = SSHManager(ip, user, psw)
+        format_print(f"{self.ecs_dict[key]} added.")
+
+    def handle_del_ecs(self, value):
+        key = value['ip']
+        del self.ecs_dict[key]
+        format_print(f"Delete {key}: {self.ecs_dict[key]}.")
+        # TODO: Del all task
+        ...
+
+    def handle_display_task(self, value):
+        ...
+
+    def handle_join_task(self, value):
+        ...
+
+    def handle_create_task(self, value):
+        ...
+
+    def handle_update_task(self, value):
+        ...
+
+    def handle_access_task(self, value):
+        ...
+
+    def handle_shut_down(self, value):
+        ...
 
 
-@Gooey(richtext_controls=True,
-       program_name="FederatedScopeCloudOrganizer",
-       encoding="utf-8",
-       progress_regex=r"^progress: (\d+)%$",
-       return_to_config=True)
-def main():
-    organizer = Celery()
-    organizer.config_from_object('cfg_client')
+def check_value(value_dict):
+    values = list(value_dict.values())
+    cnt = 0
+    for i in values:
+        if i is True:
+            cnt += 1
+
+    if cnt == 1:
+        return True
+    else:
+        return False
+
+
+def FederatedScopeCloudOrganizer():
+    # The main GUI of FederatedScopeCloudOrganizer
+    sg.theme('Gray Gray Gray')
+    a = 0
+
     # ---------------------------------------------------------------------- #
-    # Global variables
+    # ECS Manager related
     # ---------------------------------------------------------------------- #
-    ecs_dict, room_dict = dict(), dict()
-    timeout = 10
-
-    settings_msg = 'FederatedScope Cloud Organizer, powered by FS team.'
-    parser = GooeyParser(description=settings_msg)
-
-    subs = parser.add_subparsers(help='commands', dest='command')
-
-    # ---------------------------------------------------------------------- #
-    # ECS Manager
-    # ---------------------------------------------------------------------- #
-    Leftparser = subs.add_parser('ECS_Manager')
-    Leftparser.add_argument("--DisplayEcs",
-                            metavar='Display saved ECS in client control list',
-                            widget='BlockCheckbox',
-                            action="store_true",
-                            default=True)
-
-    # do_add_ecs
-    Leftparser = subs.add_parser('__Add_ECS')
-    Leftparser.add_argument("ip", metavar='ip address', default='172.X.X.X')
-    Leftparser.add_argument("user", metavar='user name of ECS', default='root')
-    Leftparser.add_argument("psw",
-                            metavar='password of user',
-                            default='123456',
-                            widget='PasswordField')
-
-    # do_del_ecs
-    Leftparser = subs.add_parser('__Del_ECS')
-    Leftparser.add_argument("ip", metavar='ip address', default='172.X.X.X')
-
-    # do_join_room
-    Leftparser = subs.add_parser('__Join_Room')
-    Leftparser.add_argument("ip", metavar='ip address', default='172.X.X.X')
-    Leftparser.add_argument("room_id", metavar='room id', default='0')
-    Leftparser.add_argument("cfg",
-                            metavar='cfg file',
-                            default='test.yaml',
-                            widget='FileChooser')
-    Leftparser.add_argument("opt", metavar='other opts', default='device 0')
+    display_ecs_layout = [[
+        sg.Text('Display saved ECS in client control '
+                'list.')
+    ], [sg.Checkbox('Enabled', default=False, k='display_ecs')]]
+    add_ecs_layout = [[sg.Text('Add ECS to client control list.')],
+                      [sg.Checkbox('Enabled', default=False, k='add_ecs')],
+                      [
+                          sg.T('IP Address', size=(8, 1)),
+                          sg.Input('172.X.X.X', key='ip', size=(35, 1))
+                      ],
+                      [
+                          sg.T('User Name', size=(8, 1)),
+                          sg.Input('root', key='user', size=(35, 1))
+                      ],
+                      [
+                          sg.T('Password', size=(8, 1)),
+                          sg.Input('123456',
+                                   password_char='*',
+                                   key='password',
+                                   size=(35, 1))
+                      ]]
+    del_ecs_layout = [[sg.Text('Delete ECS from client control list.')],
+                      [sg.Checkbox('Enabled', default=False, k='del_ecs')],
+                      [sg.Text('IP Address'),
+                       sg.Input(key='ip')]]
+    ecs_group = sg.TabGroup([[
+        sg.Tab('DisplayECS', display_ecs_layout),
+        sg.Tab('AddECS', add_ecs_layout),
+        sg.Tab('DelECS', del_ecs_layout)
+    ]])
+    ecs_layout = [[ecs_group]]
 
     # ---------------------------------------------------------------------- #
     # Task manager related
     # ---------------------------------------------------------------------- #
-    Leftparser = subs.add_parser('Room_Manager')
-    Leftparser.add_argument("--DisplayTask",
-                            metavar='Display all running tasks in client '
-                            'task list',
-                            widget='BlockCheckbox',
-                            action="store_true",
-                            default=True)
+    display_task_layout = [[
+        sg.Text('Display all running tasks in client '
+                'task list.')
+    ], [sg.Checkbox('Enabled', default=False, k='display_task')]]
+    join_task_layout = [[sg.Text('Let an ECS join a specific task.')],
+                        [sg.Checkbox('Enabled', default=False, k='join_task')],
+                        [
+                            sg.T('IP Address', size=(8, 1)),
+                            sg.Input('172.X.X.X', key='ip', size=(35, 1))
+                        ],
+                        [
+                            sg.T('Task ID', size=(8, 1)),
+                            sg.Input('0', key='task_id', size=(35, 1))
+                        ],
+                        [
+                            sg.T('YAML file', size=(8, 1)),
+                            sg.Input('', key='yaml', size=(35, 1)),
+                            sg.FilesBrowse()
+                        ],
+                        [
+                            sg.T('Opts', size=(8, 1)),
+                            sg.Input('device 0', key='opts', size=(35, 1))
+                        ]]
+    create_task_layout = [
+        [sg.Text('Create FS task in server with specific '
+                 'command.')],
+        [sg.Checkbox('Enabled', default=False, k='create_task')],
+        [
+            sg.T('YAML file', size=(8, 1)),
+            sg.Input('', key='yaml', size=(35, 1)),
+            sg.FilesBrowse()
+        ],
+        [
+            sg.T('Opts', size=(8, 1)),
+            sg.Input('device 0', key='opts', size=(35, 1))
+        ],
+        [
+            sg.T('Password', size=(8, 1)),
+            sg.Input('123456', password_char='*', key='password', size=(35, 1))
+        ],
+    ]
+    update_task_layout = [[
+        sg.Text('Fetch all FS tasks from remote server (will '
+                'forget all saved room).')
+    ], [sg.Checkbox('Enabled', default=False, k='update_task')]]
+    access_task_layout = [[sg.Text('Obtain access to a spesific task.')],
+                          [
+                              sg.Checkbox('Enabled',
+                                          default=False,
+                                          k='access_task')
+                          ]]
+    task_group = sg.TabGroup([[
+        sg.Tab('DisplayTask', display_task_layout),
+        sg.Tab('JoinTask', join_task_layout),
+        sg.Tab('CreateTask', create_task_layout),
+        sg.Tab('UpdateTask', update_task_layout),
+        sg.Tab('AccessTask', access_task_layout)
+    ]])
+    task_layout = [[task_group]]
 
-    # do_create_room
-    Leftparser = subs.add_parser('__Create_Room')
-    Leftparser.add_argument("cfg",
-                            metavar='cfg file',
-                            default='test.yaml',
-                            widget='FileChooser')
-
-    # do_update_room
-    Leftparser = subs.add_parser('__Update_Room')
-    Leftparser.add_argument("--UpdateRoom",
-                            metavar='Fetch all FS rooms from Lobby (will '
-                            'forget all saved room)',
-                            widget='BlockCheckbox',
-                            action="store_true",
-                            default=True)
-
-    # do_access_room
-    Leftparser = subs.add_parser('__Access_Room')
-    Leftparser.add_argument("id", metavar='room id', default='0')
-    Leftparser.add_argument("psw", metavar='password', default='123456')
-    Leftparser.add_argument("verbose",
-                            metavar='verbose',
-                            widget="Listbox",
-                            nargs="*",
-                            choices=[0, 1, 2],
-                            default=[0],
-                            help='    0: print no information\n'
-                            '    1: print information of a specific room\n'
-                            '    2: print information of all the rooms')
     # ---------------------------------------------------------------------- #
-    # Server manager related
+    # Server related messages
     # ---------------------------------------------------------------------- #
-    Leftparser = subs.add_parser('Server_Manager')
-    Leftparser.add_argument("--ShutDown",
-                            metavar='Shut down all rooms and quit',
-                            widget='BlockCheckbox',
-                            action="store_true",
-                            default=True)
+    shut_down_layout = [[sg.Text('Shut down all rooms and quit')],
+                        [sg.Checkbox('Enabled', default=False, k='shut_down')]]
 
-    args = parser.parse_args()
+    server_group = sg.TabGroup([[sg.Tab('ShutDown', shut_down_layout)]])
 
-    if args.command == 'ECS_Manager':
+    server_layout = [[server_group]]
+
+    main_layout = [[
+        sg.TabGroup([[
+            sg.Tab('ECSManager', ecs_layout),
+            sg.Tab('TaskManager', task_layout),
+            sg.Tab('ServerManager', server_layout)
+        ]])
+    ]]
+
+    # ---------------------------------------------------------------------- #
+    # Main layout
+    # ---------------------------------------------------------------------- #
+    output_layout = [[sg.Text('INFO:')], [sg.Output(size=(100, 15))]]
+
+    layout = [[sg.Text('FederatedScope Cloud Organizer, powered by FS team.')],
+              [sg.Frame('Actions', layout=main_layout)], output_layout,
+              [sg.Button('RUN', bind_return_key=True)]]
+
+    window = sg.Window('FederatedScopeCloudOrganizer',
+                       layout,
+                       default_element_size=(30, 2),
+                       finalize=True)
+
+    handler = UIEventHandler()
+
+    while True:
         try:
-            info = ""
-            for key, value in ecs_dict.items():
-                info += f"ecs: {key}, info: {value}\n"
-            if info:
-                format_print(info)
-            else:
-                format_print('No saved ECS, please add ECS via `__Add_ECS` '
-                             'first!')
+            event, values = window.read()
+            if event == 'RUN':
+                if not check_value(values):
+                    format_print('There are no executable commands or '
+                                 'multiple commands. Please check the '
+                                 'CheckBox.')
+                    continue
+
+                for k, v in values.items():
+                    if v is True:
+                        if hasattr(handler, f'handle_{k}'):
+                            getattr(handler, f'handle_{k}')(values)
+                        else:
+                            format_print(f'No valid handle_{k}.')
+
+            elif event == sg.WIN_CLOSED:  # always,  always give a way out!
+                break
         except Exception as error:
             format_print(f"Exception: {error}")
-    elif args.command == '__Add_ECS':
-        try:
-            ip, user, psw = args.ip, args.user, args.psw
-            key = f"{ip}"
-            if key in ecs_dict:
-                raise ValueError(f"ECS `{key}` already exists.")
-            ecs_dict[key] = SSHManager(ip, user, psw)
-            format_print(f"{ecs_dict[key]} added.")
-        except Exception as error:
-            format_print(f"Exception: {error}")
-    elif args.command == '__Del_ECS':
-        try:
-            ip = args.ip
-            format_print(f"Delete {ip}: {ecs_dict[ip]}.")
-            # TODO: Del all task
-            del ecs_dict[ip]
-        except Exception as error:
-            format_print(f"Exception: {error}")
-    elif args.command == '__Join_Room':
-        try:
-            ...
-        except Exception as error:
-            format_print(f"Exception: {error}")
-    elif args.command == 'Room_Manager':
-        try:
-            ...
-        except Exception as error:
-            format_print(f"Exception: {error}")
-    elif args.command == '__Create_Room':
-        try:
-            ...
-        except Exception as error:
-            format_print(f"Exception: {error}")
-    elif args.command == '__Update_Room':
-        try:
-            ...
-        except Exception as error:
-            format_print(f"Exception: {error}")
-    elif args.command == '__Access_Room':
-        try:
-            ...
-        except Exception as error:
-            format_print(f"Exception: {error}")
-    elif args.command == 'Server_Manager':
-        try:
-            ...
-        except Exception as error:
-            format_print(f"Exception: {error}")
-    else:
-        print("Thanks for using FederatedScopeCloudOrganizer, bye!")
-        time.sleep(1)
-        return
+
+    window.close()
 
 
 if __name__ == '__main__':
-    main()
+    FederatedScopeCloudOrganizer()
