a434410ac59adcdb95ac26eb1c2250abe29aad33
rayrayraykk <18007356109@163.com>
2023-01-12 19:19:51 +0800

    fix scripts

--- scripts/hpo_exp_scripts/robustness/attack/run.sh
+++ scripts/hpo_exp_scripts/robustness/attack/run.sh
@@ -9,5 +9,5 @@ methods=('rs_wrap' 'bo_gp_wrap' 'bo_kde_wrap' 'bo_rf_wrap' 'hb_wrap' 'bohb_wrap'
 
 for (( m=0; m<${#methods[@]}; m++ ))
 do
-	nohup python federatedscope/hpo.py --cfg scripts/hpo_exp_scripts/uniqueness/cifar10/${methods[$m]}.yaml device $((m%4)) seed ${seed} hpo.fedex.attack.id ${attacker} hpo.fedex.attack.sigma ${sigma} >/dev/null 2>&1 &
+	nohup python federatedscope/hpo.py --cfg scripts/hpo_exp_scripts/robustness/attack/${methods[$m]}.yaml device $((m%4)) seed ${seed} hpo.fedex.attack.id ${attacker} hpo.fedex.attack.sigma ${sigma} >/dev/null 2>&1 &
 done
