3cc47b7dbd4c31c99507d536769ffc32e5e8fa62
rayrayraykk <18007356109@163.com>
2023-01-16 14:27:36 +0800

    fix yaml for hpo

---
+++ scripts/hpo_exp_scripts/parse_exp_results.py
@@ -0,0 +1,3 @@
+# TODO: implement this
+def parse():
+    ...
--- scripts/hpo_exp_scripts/uniqueness/cifar10/run.sh
+++ scripts/hpo_exp_scripts/uniqueness/cifar10/run.sh
@@ -15,11 +15,12 @@ set -e
 
 alpha=$1
 seed=$2
+device=$3
 
 methods=('rs' 'rs_wrap' 'bo_gp' 'bo_gp_wrap' 'bo_kde' 'bo_kde_wrap' 'bo_rf' 'bo_rf_wrap' 'hb' 'hb_wrap' 'bohb' 'bohb_wrap')
 
 
 for (( m=0; m<${#methods[@]}; m++ ))
 do
-	nohup python federatedscope/hpo.py --cfg scripts/hpo_exp_scripts/uniqueness/cifar10/${methods[$m]}.yaml device $((m%4)) seed ${seed} data.splitter_args "[{'alpha': ${alpha}}]" >/dev/null 2>&1 &
+	nohup python federatedscope/hpo.py --cfg scripts/hpo_exp_scripts/uniqueness/cifar10/${methods[$m]}.yaml device $((m%${device})) seed ${seed} data.splitter_args "[{'alpha': ${alpha}}]" >/dev/null 2>&1 &
 done
