81482d0e8a193496357aa7b5434e92888730e8dd
rayrayraykk <18007356109@163.com>
2023-01-17 12:19:41 +0800

    add run

--- scripts/hpo_exp_scripts/robustness/attack/run.sh
+++ scripts/hpo_exp_scripts/robustness/attack/run.sh
@@ -1,5 +1,17 @@
 set -e
 
+#bash scripts/hpo_exp_scripts/robustness/attack/run.sh "[1,2,3,4]" 1.0 12345 8
+#bash scripts/hpo_exp_scripts/robustness/attack/run.sh "[1,2,3,4]" 1.0 12346 8
+#bash scripts/hpo_exp_scripts/robustness/attack/run.sh "[1,2,3,4]" 1.0 12347 8
+#
+#bash scripts/hpo_exp_scripts/robustness/attack/run.sh "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]" 1.0 12345 8
+#bash scripts/hpo_exp_scripts/robustness/attack/run.sh "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]" 1.0 12346 8
+#bash scripts/hpo_exp_scripts/robustness/attack/run.sh "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]" 1.0 12347 8
+#
+#bash scripts/hpo_exp_scripts/robustness/attack/run.sh "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64]" 1.0 12345 8
+#bash scripts/hpo_exp_scripts/robustness/attack/run.sh "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64]" 1.0 12346 8
+#bash scripts/hpo_exp_scripts/robustness/attack/run.sh "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64]" 1.0 12347 8
+
 attacker=$1
 sigma=$2
 seed=$3
