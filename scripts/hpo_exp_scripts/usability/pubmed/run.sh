58d3af4ffeaa5cf5d318e1088adc049425cee38d
rayrayraykk <18007356109@163.com>
2023-01-17 12:05:02 +0800

    update scripts

--- scripts/hpo_exp_scripts/usability/pubmed/run.sh
+++ scripts/hpo_exp_scripts/usability/pubmed/run.sh
@@ -8,5 +8,5 @@ methods=('rs' 'rs_wrap' 'bo_gp' 'bo_gp_wrap' 'bo_kde' 'bo_kde_wrap' 'bo_rf' 'bo_
 
 for (( m=0; m<${#methods[@]}; m++ ))
 do
-	nohup python federatedscope/hpo.py --cfg scripts/hpo_exp_scripts/usability/pubmed/${methods[$m]}.yaml device $((m%$((m%${device})))) seed ${seed} >/dev/null 2>&1
+	nohup python federatedscope/hpo.py --cfg scripts/hpo_exp_scripts/usability/pubmed/${methods[$m]}.yaml device $((m%${device})) seed ${seed} >/dev/null 2>&1
 done
