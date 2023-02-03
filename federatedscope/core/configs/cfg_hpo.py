c9f8bdff49d5005aca8c495d3530dd091b8b9132
815961618@qq.com <815961618@qq.com>
2023-02-03 15:19:30 +0800

    fix config

--- federatedscope/core/configs/cfg_hpo.py
+++ federatedscope/core/configs/cfg_hpo.py
@@ -50,6 +50,34 @@ def extend_hpo_cfg(cfg):
     cfg.hpo.table.num = 27
     cfg.hpo.table.idx = 0
 
+    # FTS
+    cfg.hpo.fts = CN()
+    cfg.hpo.fts.use = False
+    cfg.hpo.fts.ss = ''
+    cfg.hpo.fts.target_clients = []
+    cfg.hpo.fts.diff = False
+    cfg.hpo.fts.local_bo_max_iter = 50
+    cfg.hpo.fts.local_bo_epochs = 50
+    cfg.hpo.fts.fed_bo_max_iter = 50
+    cfg.hpo.fts.ls = 1.0
+    cfg.hpo.fts.var = 0.1
+    cfg.hpo.fts.g_var = 1e-6
+    cfg.hpo.fts.v_kernel = 1.0
+    cfg.hpo.fts.obs_noise = 1e-6
+    cfg.hpo.fts.M = 100
+    cfg.hpo.fts.M_target = 200
+    cfg.hpo.fts.gp_opt_schedule = 1
+    cfg.hpo.fts.allow_load_existing_info = True
+
+    # pfedhpo
+    cfg.hpo.pfedhpo = CN()
+    cfg.hpo.pfedhpo.use = False
+    cfg.hpo.pfedhpo.discrete = False
+    cfg.hpo.pfedhpo.train_fl = False
+    cfg.hpo.pfedhpo.train_anchor = False
+    cfg.hpo.pfedhpo.ss = ''
+    cfg.hpo.pfedhpo.target_fl_total_round = 1000
+
 
 def assert_hpo_cfg(cfg):
     # HPO related
@@ -82,6 +110,9 @@ def assert_hpo_cfg(cfg):
                                                        "adopted, " \
                                                        "federate.use_diff " \
                                                        "must be True."
+
+    assert cfg.hpo.fts.use == cfg.federate.use_diff, \
+        "Once FTS is adopted, federate.use_diff must be True."
 
 
 register_config("hpo", extend_hpo_cfg)
