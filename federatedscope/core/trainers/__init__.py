6375c36ea016657a0d4538c7c2a3dbc79c84bcc8
Daoyuan Chen <67475544+yxdyc@users.noreply.github.com>
2023-04-03 19:08:38 +0800

    Update __init__.py

--- federatedscope/core/trainers/__init__.py
+++ federatedscope/core/trainers/__init__.py
@@ -8,7 +8,8 @@ from federatedscope.core.trainers.trainer_pFedMe import wrap_pFedMeTrainer
 from federatedscope.core.trainers.trainer_Ditto import wrap_DittoTrainer
 from federatedscope.core.trainers.trainer_FedEM import FedEMTrainer
 from federatedscope.core.trainers.trainer_FedRep import wrap_FedRepTrainer
-from federatedscope.core.trainers.trainer_simple_tuning import wrap_Simple_tuning_Trainer
+from federatedscope.core.trainers.trainer_simple_tuning import \
+    wrap_Simple_tuning_Trainer
 from federatedscope.core.trainers.context import Context
 from federatedscope.core.trainers.trainer_fedprox import wrap_fedprox_trainer
 from federatedscope.core.trainers.trainer_nbafl import wrap_nbafl_trainer, \
