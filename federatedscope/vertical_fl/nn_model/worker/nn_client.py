58302644dceb13853d281e6e6ed46922d2cce1a0
qbc <qianbingchen.qbc@alibaba-inc.com>
2023-04-17 17:56:04 +0800

    plaintext version

--- federatedscope/vertical_fl/nn_model/worker/nn_client.py
+++ federatedscope/vertical_fl/nn_model/worker/nn_client.py
@@ -1,6 +1,10 @@
 import numpy as np
 import logging
 
+import torch
+from torch import nn, optim
+from torch.autograd import Variable
+
 from federatedscope.core.workers import Client
 from federatedscope.core.message import Message
 from federatedscope.vertical_fl.dataloader.utils import batch_iter
@@ -50,10 +54,6 @@ class nnClient(Client):
         self.dataloader = batch_iter(self.data['train'],
                                      self._cfg.dataloader.batch_size,
                                      shuffled=True)
-        self.w1 = np.random.randn(self.feature_num)
-        self.w2 = np.random.randn(self.feature_num)
-        if self.own_label:
-            self.final_w = np.random.randn(4)
 
     def sample_data(self, index=None):
         if index is None:
@@ -74,10 +74,31 @@ class nnClient(Client):
         return fx * (1 - fx)
 
     def callback_funcs_for_model_para(self, message: Message):
+        self.bottom_model = nn.Sequential(
+            nn.Linear(self.dims[self.ID] - self.dims[self.ID - 1], 3),
+            nn.ReLU())
+        self.bottom_model_opt = optim.SGD(self.bottom_model.parameters(),
+                                          lr=self.lr)
+        self.bottom_model_opt.zero_grad()
 
         if self.own_label:
+            self.top_model = nn.Sequential(nn.Linear(6, 1), nn.Sigmoid())
+            self.top_model_opt = optim.SGD(self.top_model.parameters(),
+                                           lr=self.lr)
+            self.top_model_opt.zero_grad()
+
+            self.models = [self.bottom_model, self.top_model]
+
             index, self.batch_x, self.batch_y = self.sample_data()
             self.batch_index = index
+
+            self.batch_y = np.vstack(self.batch_y).reshape(-1, 1)
+
+            self.batch_x = torch.from_numpy(self.batch_x)
+            self.batch_y = torch.from_numpy(self.batch_y)
+
+            self.batch_x = self.batch_x.to(torch.float32)
+            self.batch_y = self.batch_y.to(torch.float32)
 
             self.comm_manager.send(
                 Message(msg_type='sample_data',
@@ -93,62 +114,49 @@ class nnClient(Client):
     def callback_func_for_sample_data(self, message: Message):
         self.batch_index = message.content
         self.batch_x = self.sample_data(index=self.batch_index)
+        self.batch_x = torch.from_numpy(self.batch_x)
+        self.batch_x = self.batch_x.to(torch.float32)
         self.compute_local_nn()
 
     def compute_local_nn(self):
-        self.h1 = np.matmul(self.batch_x, self.w1)
-        self.h2 = np.matmul(self.batch_x, self.w2)
+
+        self.a = self.bottom_model(self.batch_x)
+        a = self.a.detach().requires_grad_()
+        # print('----')
+        # print(a)
+        # print(self.a)
         if not self.own_label:
             self.comm_manager.send(
                 Message(msg_type='middle_result',
                         sender=self.ID,
                         receiver=self.client_num,
                         state=self.state,
-                        content=(self.h1, self.h2)))
+                        content=a))
 
     def callback_func_for_middle_result(self, message: Message):
-        h00, h01 = message.content
-        n1 = self.sigmoid(h00)
-        n2 = self.sigmoid(h01)
-        n3 = self.sigmoid(self.h1)
-        n4 = self.sigmoid(self.h2)
-        res = 0
-        n = [n1, n2, n3, n4]
-        for i in range(len(self.final_w)):
-            res += self.final_w[i] * n[i]
+        other_a = message.content
+        # a = other_a
+        a = torch.cat((other_a, self.a), 1)
+        a = Variable(a, requires_grad=True)
+        # print("===")
+        # print(a.requires_grad_())
+        # print(a)
+        y_hat = self.top_model(a)
+        # print(y_hat)
+        # print(self.batch_y)
 
-        if self._cfg.criterion.type == 'CrossEntropyLoss':
-            y_pred = self.sigmoid(res)
-        else:
-            y_pred = res
-        loss = self.mse_loss(self.batch_y, y_pred)
+        criterion = nn.MSELoss()
+        loss = criterion(y_hat, self.batch_y)
+        # print(loss)
+        loss.backward()
 
-        d_L_d_ypred = -2 * (self.batch_y - y_pred)
-        if self._cfg.criterion.type == 'CrossEntropyLoss':
-            d_ypred_d_final_w = self.deriv_sigmoid(res) * np.asarray(
-                [n1, n2, n3, n4])
-        else:
-            d_ypred_d_final_w = np.asarray([n1, n2, n3, n4])
+        other_grad = a.grad[:, :3]
+        my_grad = a.grad[:, 3:]
+        # print(a.grad)
+        # print(other_grad)
 
-        d_ypred_d_n1 = self.deriv_sigmoid(res) * self.final_w[0]
-        d_ypred_d_n2 = self.deriv_sigmoid(res) * self.final_w[1]
-        d_ypred_d_n3 = self.deriv_sigmoid(res) * self.final_w[2]
-        d_ypred_d_n4 = self.deriv_sigmoid(res) * self.final_w[3]
-
-        # d_n3_d_w = np.matmul(self.deriv_sigmoid(self.h1), self.batch_x)
-        # d_n4_d_w = np.matmul(self.deriv_sigmoid(self.h2), self.batch_x)
-
-        self.final_w -= self.lr * np.matmul(d_ypred_d_final_w, d_L_d_ypred)
-
-        self.w1 -= self.lr * np.matmul(
-            self.deriv_sigmoid(self.h1) * d_ypred_d_n3 * d_L_d_ypred,
-            self.batch_x)
-        self.w2 -= self.lr * np.matmul(
-            self.deriv_sigmoid(self.h2) * d_ypred_d_n4 * d_L_d_ypred,
-            self.batch_x)
-
-        d_L_d_n1 = d_ypred_d_n1 * d_L_d_ypred
-        d_L_d_n2 = d_ypred_d_n2 * d_L_d_ypred
+        self.a.backward(my_grad)
+        self.bottom_model_opt.step()
 
         self.comm_manager.send(
             Message(msg_type='derivative',
@@ -158,17 +166,25 @@ class nnClient(Client):
                         if each != self.server_id
                     ],
                     state=self.state,
-                    content=(d_L_d_n1, d_L_d_n2)))
+                    content=other_grad))
+
+        # print(self.top_model_opt)
+        self.top_model_opt.step()
+        # print(self.top_model_opt)
 
     def callback_func_for_derivative(self, message: Message):
-        d_L_d_n1, d_L_d_n2 = message.content
-        # d_n1_d_w = np.matmul(self.deriv_sigmoid(self.h1), self.batch_x)
-        # d_n2_d_w = np.matmul(self.deriv_sigmoid(self.h1), self.batch_x)
+        grad = message.content
+        # print("==========")
+        # print(grad)
+        # # grad = torch.from_numpy(grad)
+        # print('-----')
 
-        self.w1 -= self.lr * np.matmul(
-            self.deriv_sigmoid(self.h1) * d_L_d_n1, self.batch_x)
-        self.w2 -= self.lr * np.matmul(
-            self.deriv_sigmoid(self.h1) * d_L_d_n2, self.batch_x)
+        # print(self.a)
+        # print(grad)
+        # input()
+        self.a.backward(grad)
+        # print(self.bottom_model[0].weight.grad)
+        self.bottom_model_opt.step()
 
         self.comm_manager.send(
             Message(msg_type='continue',
@@ -192,13 +208,21 @@ class nnClient(Client):
                         state=self.state,
                         content='None'))
             self.comm_manager.send(
-                Message(msg_type='para',
+                Message(msg_type='model',
                         sender=self.ID,
                         receiver=[self.server_id],
                         state=self.state,
-                        content=(self.w1, self.w2, self.final_w)))
+                        content=self.models))
             index, self.batch_x, self.batch_y = self.sample_data()
             self.batch_index = index
+
+            self.batch_y = np.vstack(self.batch_y).reshape(-1, 1)
+
+            self.batch_x = torch.from_numpy(self.batch_x)
+            self.batch_y = torch.from_numpy(self.batch_y)
+
+            self.batch_x = self.batch_x.to(torch.float32)
+            self.batch_y = self.batch_y.to(torch.float32)
 
             self.comm_manager.send(
                 Message(msg_type='sample_data',
@@ -213,6 +237,14 @@ class nnClient(Client):
         elif self.state < self._cfg.federate.total_round_num:
             index, self.batch_x, self.batch_y = self.sample_data()
             self.batch_index = index
+
+            self.batch_y = np.vstack(self.batch_y).reshape(-1, 1)
+
+            self.batch_x = torch.from_numpy(self.batch_x)
+            self.batch_y = torch.from_numpy(self.batch_y)
+
+            self.batch_x = self.batch_x.to(torch.float32)
+            self.batch_y = self.batch_y.to(torch.float32)
 
             self.comm_manager.send(
                 Message(msg_type='sample_data',
@@ -235,16 +267,16 @@ class nnClient(Client):
                         state=self.state,
                         content='None'))
             self.comm_manager.send(
-                Message(msg_type='para',
+                Message(msg_type='model',
                         sender=self.ID,
                         receiver=[self.server_id],
                         state=self.state,
-                        content=(self.w1, self.w2, self.final_w)))
+                        content=self.models))
 
     def callback_func_for_eval(self, message: Message):
         self.comm_manager.send(
-            Message(msg_type='para',
+            Message(msg_type='model',
                     sender=self.ID,
                     receiver=[self.server_id],
                     state=self.state,
-                    content=(self.w1, self.w2)))
+                    content=self.bottom_model))
--- federatedscope/vertical_fl/nn_model/worker/nn_server.py
+++ federatedscope/vertical_fl/nn_model/worker/nn_server.py
@@ -1,6 +1,8 @@
 import numpy as np
 import logging
 
+import torch
+
 from federatedscope.core.workers import Server
 from federatedscope.core.message import Message
 from federatedscope.vertical_fl.Paillier import abstract_paillier
@@ -27,14 +29,14 @@ class nnServer(Server):
         super(nnServer,
               self).__init__(ID, state, config, data, model, client_num,
                              total_round_num, device, strategy, **kwargs)
-        self.w_dict = dict()
+        self.model_dict = dict()
         cfg_key_size = config.vertical.key_size
         self.public_key, self.private_key = \
             abstract_paillier.generate_paillier_keypair(n_length=cfg_key_size)
         self.vertical_dims = config.vertical.dims
         self._init_data_related_var()
 
-        self.register_handlers('para', self.callback_func_for_para)
+        self.register_handlers('model', self.callback_func_for_model)
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
@@ -45,6 +47,14 @@ class nnServer(Server):
         self.theta = self.model.state_dict()['fc.weight'].numpy().reshape(-1)
         self.test_x = self.data['test']['x']
         self.test_y = self.data['test']['y']
+
+        self.test_x = self.test_x[:, :]
+        self.test_x = torch.from_numpy(self.test_x)
+        self.test_x = self.test_x.to(torch.float32)
+
+        self.test_y = np.vstack(self.test_y).reshape(-1, 1)
+        self.test_y = torch.from_numpy(self.test_y)
+        self.test_y = self.test_y.to(torch.float32)
 
     def trigger_for_start(self):
         if self.check_client_join_in():
@@ -59,33 +69,21 @@ class nnServer(Server):
                     state=self.state,
                     content='None'))
 
-    def callback_func_for_para(self, message: Message):
-        if message.sender == 1:
-            w1, w2 = message.content
-            self.w_dict[1] = [w1, w2]
-        elif message.sender == 2:
-            w1, w2, self.w = message.content
-            self.w_dict[2] = [w1, w2]
-        if len(self.w_dict) == 2:
-            # print(self.w)
-            h1 = self.sigmoid(
-                np.matmul(self.test_x[:, :self.dims[1]], self.w_dict[1][0]))
-            h2 = self.sigmoid(
-                np.matmul(self.test_x[:, :self.dims[1]], self.w_dict[1][1]))
-            h3 = self.sigmoid(
-                np.matmul(self.test_x[:, self.dims[1]:], self.w_dict[2][0]))
-            h4 = self.sigmoid(
-                np.matmul(self.test_x[:, self.dims[1]:], self.w_dict[2][1]))
-            h = [h1, h2, h3, h4]
-            res = 0
-            for i in range(4):
-                res += self.w[i] * h[i]
-            self.w_dict = dict()
-            if self._cfg.criterion.type == 'CrossEntropyLoss':
-                y_hat = self.sigmoid(res)
-            else:
-                y_hat = res
-            loss = np.mean((self.test_y - y_hat)**2)
-            y_hat = (y_hat >= 0.5).astype(np.float32)
-            acc = np.sum(y_hat == self.test_y) / len(self.test_y)
+    def callback_func_for_model(self, message: Message):
+        self.model_dict[message.sender] = message.content
+
+        if len(self.model_dict) == 2:
+            # print(self.dims)
+            # print(self.test_x[:, :self.dims[1]])
+            a1 = self.model_dict[1](self.test_x[:, :self.dims[1]])
+
+            a2 = self.model_dict[2][0](self.test_x[:, self.dims[1]:])
+            a = torch.cat((a1, a2), 1)
+            y_hat = self.model_dict[2][1](a)
+            self.model_dict = dict()
+            # print(y_hat)
+            # print(self.test_y)
+            loss = torch.mean((self.test_y - y_hat)**2)
+            y_hat = (y_hat >= 0.5)
+            acc = torch.sum(y_hat == self.test_y) / len(self.test_y)
             print(loss, acc)
