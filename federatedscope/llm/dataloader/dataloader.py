61e4b51ccefbdd088e1e9d4c845e9434d27ad940
Weirui Kuang <39145382+rayrayraykk@users.noreply.github.com>
2023-05-15 11:52:06 +0800

    Init for LLM (#592)

---
+++ federatedscope/llm/dataloader/dataloader.py
@@ -0,0 +1,105 @@
+import os
+import torch
+import transformers
+
+from dataclasses import dataclass
+from federatedscope.llm.dataset.llm_dataset import DefaultToken, LLMDataset
+
+
+@dataclass
+class LLMDataCollator(object):
+    """Collate examples for supervised fine-tuning."""
+
+    tokenizer: transformers.PreTrainedTokenizer
+
+    def __call__(self, instances):
+        input_ids, labels = tuple([instance[key] for instance in instances]
+                                  for key in ("input_ids", "labels"))
+        input_ids = torch.nn.utils.rnn.pad_sequence(
+            input_ids,
+            batch_first=True,
+            padding_value=self.tokenizer.pad_token_id)
+        labels = torch.nn.utils.rnn.pad_sequence(
+            labels,
+            batch_first=True,
+            padding_value=DefaultToken.IGNORE_INDEX.value)
+        return dict(
+            input_ids=input_ids,
+            labels=labels,
+            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
+        )
+
+
+def get_tokenizer(model_name, cache_dir, tok_len=128):
+    from transformers import AutoTokenizer
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name,
+        cache_dir=cache_dir,
+        model_max_length=tok_len,
+        padding_side="right",
+        use_fast=False,
+    )
+
+    special_tokens = dict()
+    if tokenizer.pad_token is None:
+        special_tokens["pad_token"] = DefaultToken.PAD_TOKEN.value
+    if tokenizer.eos_token is None:
+        special_tokens["eos_token"] = DefaultToken.EOS_TOKEN.value
+    if tokenizer.bos_token is None:
+        special_tokens["bos_token"] = DefaultToken.BOS_TOKEN.value
+    if tokenizer.unk_token is None:
+        special_tokens["unk_token"] = DefaultToken.UNK_TOKEN.value
+
+    num_new_tokens = tokenizer.add_special_tokens(special_tokens)
+
+    return tokenizer, num_new_tokens
+
+
+def load_llm_dataset(config=None, **kwargs):
+    model_name, _ = config.model.type.split('@')
+
+    # Resize the model
+    tokenizer, num_new_tokens = \
+        get_tokenizer(model_name, config.data.root, config.llm.tok_len)
+
+    # The data format is supposed to be a json file
+    # Example: config.data.type: xxx.json@llm
+    dataset_name, _ = config.data.type.split('@')
+    fp = os.path.join(config.data.root, dataset_name)
+    dataset = LLMDataset(fp, tokenizer)
+    data_collator = LLMDataCollator(tokenizer=tokenizer)
+
+    return dataset, data_collator, tokenizer, num_new_tokens
+
+
+if __name__ == '__main__':
+    # Test cases
+    from federatedscope.core.configs.config import CN
+
+    config = CN()
+    config.seed = 42
+
+    config.model = CN()
+    config.model.type = 'gpt2@huggingface_llm'
+
+    config.llm = CN()
+    config.llm.tok_len = 1000
+
+    config.llm.dataset = CN()
+    config.llm.dataset.source = ['instruction', input]
+    config.llm.dataset.target = ['output']
+
+    config.data = CN()
+    config.data.root = 'data'
+    config.data.type = 'alpaca_data.json@llm'
+    config.data.splits = [0, 0.5, 0.5]
+
+    dataset, data_collator, tokenizer, num_new_tokens = \
+        load_llm_dataset(config)
+
+    cnt = 10
+    for i, data in enumerate(dataset):
+        print(data)
+        if cnt < i:
+            break
---
+++ federatedscope/llm/dataset/llm_dataset.py
@@ -0,0 +1,100 @@
+import json
+import copy
+import logging
+
+from enum import Enum
+from torch.utils.data import Dataset
+
+logger = logging.getLogger(__name__)
+
+
+class DefaultToken(Enum):
+    PAD_TOKEN = "[PAD]"
+    EOS_TOKEN = "</s>"
+    BOS_TOKEN = "<s>"
+    UNK_TOKEN = "<unk>"
+    IGNORE_INDEX = -100
+
+
+PROMPT_DICT = {
+    "prompt_input": (
+        "Below is an instruction that describes a task, "
+        "paired with an input that provides further context. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{instruction}\n\n### Input:"
+        "\n{input}\n\n### Response:"),
+    "prompt_no_input": (
+        "Below is an instruction that describes a task. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{instruction}\n\n### Response:"),
+}
+
+
+class LLMDataset(Dataset):
+    """Dataset for supervised fine-tuning."""
+    def __init__(self, data_path, tokenizer):
+        super(LLMDataset, self).__init__()
+        with open(data_path, 'r') as f:
+            list_data_dict = json.load(f)
+
+        prompt_input, prompt_no_input = PROMPT_DICT[
+            "prompt_input"], PROMPT_DICT["prompt_no_input"]
+        sources = [
+            prompt_input.format_map(example) if example.get("input", "") != ""
+            else prompt_no_input.format_map(example)
+            for example in list_data_dict
+        ]
+        targets = [
+            f"{example['output']}{tokenizer.eos_token}"
+            for example in list_data_dict
+        ]
+
+        data_dict = self.preprocess(sources, targets, tokenizer)
+
+        self.input_ids = data_dict["input_ids"]
+        self.labels = data_dict["labels"]
+
+    def _tokenize_fn(self, strings, tokenizer):
+        """Tokenize a list of strings."""
+        tokenized_list = [
+            tokenizer(
+                text,
+                return_tensors="pt",
+                padding="longest",
+                max_length=tokenizer.model_max_length,
+                truncation=True,
+            ) for text in strings
+        ]
+        input_ids = labels = [
+            tokenized.input_ids[0] for tokenized in tokenized_list
+        ]
+        input_ids_lens = labels_lens = [
+            tokenized.input_ids.ne(tokenizer.pad_token_id).sum().item()
+            for tokenized in tokenized_list
+        ]
+        return dict(
+            input_ids=input_ids,
+            labels=labels,
+            input_ids_lens=input_ids_lens,
+            labels_lens=labels_lens,
+        )
+
+    def preprocess(self, sources, targets, tokenizer):
+        """Preprocess the data by tokenizing."""
+        examples = [s + t for s, t in zip(sources, targets)]
+        examples_tokenized, sources_tokenized = [
+            self._tokenize_fn(strings, tokenizer)
+            for strings in (examples, sources)
+        ]
+        input_ids = examples_tokenized["input_ids"]
+        labels = copy.deepcopy(input_ids)
+        for label, source_len in zip(labels,
+                                     sources_tokenized["input_ids_lens"]):
+            label[:source_len] = DefaultToken.IGNORE_INDEX.value
+        return dict(input_ids=input_ids, labels=labels)
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, i):
+        return dict(input_ids=self.input_ids[i], labels=self.labels[i])
