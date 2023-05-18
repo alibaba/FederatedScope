a8ae79bcc1cd184a31f2fab2aae3c87a947e0c75
rayrayraykk <18007356109@163.com>
2023-05-18 16:44:38 +0800

    add data para

--- federatedscope/core/trainers/torch_trainer.py
+++ federatedscope/core/trainers/torch_trainer.py
@@ -98,6 +98,8 @@ class GeneralTorchTrainer(Trainer):
         return self.ctx.eval_metrics
 
     def register_default_hooks_train(self):
+        self.register_hook_in_train(self._hook_on_data_parallel_init,
+                                    "on_fit_start")
         self.register_hook_in_train(self._hook_on_fit_start_init,
                                     "on_fit_start")
         self.register_hook_in_train(
@@ -118,6 +120,8 @@ class GeneralTorchTrainer(Trainer):
         self.register_hook_in_train(self._hook_on_fit_end, "on_fit_end")
 
     def register_default_hooks_ft(self):
+        self.register_hook_in_ft(self._hook_on_data_parallel_init,
+                                 "on_fit_start")
         self.register_hook_in_ft(self._hook_on_fit_start_init, "on_fit_start")
         self.register_hook_in_ft(self._hook_on_fit_start_calculate_model_size,
                                  "on_fit_start")
@@ -137,6 +141,8 @@ class GeneralTorchTrainer(Trainer):
 
     def register_default_hooks_eval(self):
         # test/val
+        self.register_hook_in_eval(self._hook_on_data_parallel_init,
+                                   "on_fit_start")
         self.register_hook_in_eval(self._hook_on_fit_start_init,
                                    "on_fit_start")
         self.register_hook_in_eval(self._hook_on_epoch_start, "on_epoch_start")
@@ -146,6 +152,15 @@ class GeneralTorchTrainer(Trainer):
                                    "on_batch_forward")
         self.register_hook_in_eval(self._hook_on_batch_end, "on_batch_end")
         self.register_hook_in_eval(self._hook_on_fit_end, "on_fit_end")
+
+    def _hook_on_data_parallel_init(self, ctx):
+        if isinstance(ctx.model, torch.nn.DataParallel):
+            return
+
+        if len(ctx.cfg.train.data_para_dids):
+            ctx.model = \
+                torch.nn.DataParallel(ctx.model,
+                                      device_ids=ctx.cfg.train.data_para_dids)
 
     def _hook_on_fit_start_init(self, ctx):
         """
