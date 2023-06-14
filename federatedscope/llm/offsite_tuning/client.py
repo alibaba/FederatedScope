e5bbf204f2d74e6963dcd220a46ee9fa2d0d34c2
Harli <harliwu@outlook.com>
2023-06-15 05:38:01 +0800

    Add plug-in model evaluation in offsite-tuning

--- federatedscope/llm/offsite_tuning/client.py
+++ federatedscope/llm/offsite_tuning/client.py
@@ -2,7 +2,8 @@ import logging
 
 from federatedscope.core.message import Message
 from federatedscope.core.workers.client import Client
-from federatedscope.core.auxiliaries.utils import b64deserializer
+from federatedscope.core.auxiliaries.utils import b64deserializer, \
+    merge_dict_of_results
 from federatedscope.core.auxiliaries.trainer_builder import get_trainer
 
 logger = logging.getLogger(__name__)
@@ -33,6 +34,9 @@ class OffsiteTuningClient(Client):
         self.register_handlers('emulator_and_adapter',
                                self.callback_funcs_for_emulator_and_adapter,
                                [None])
+        self.register_handlers('eval_offsite_tuning',
+                               self.callback_funcs_for_evaluate_offsite,
+                               'metrics')
 
     def callback_funcs_for_emulator_and_adapter(self, message: Message):
         logger.info(f'Client {self.ID}: Emulator and adapter received.')
@@ -44,3 +48,83 @@ class OffsiteTuningClient(Client):
                                    config=self._cfg,
                                    is_attacker=self.is_attacker,
                                    monitor=self._monitor)
+
+    def callback_funcs_for_evaluate_offsite(self, message: Message):
+        """
+        The handling function for receiving the request of
+        evaluating offsite tuning
+
+        Arguments:
+            message: The received message
+        """
+        sender, timestamp = message.sender, message.timestamp
+        self.state = message.state
+        # The content contains two parts, namely, raw/plugin model
+        # and emulator model
+
+        # Extract the raw/plugin model
+        raw_model = b64deserializer(message.content[0], tool='dill')
+        raw_model_trainer = get_trainer(model=raw_model,
+                                        data=self.data,
+                                        device=self.device,
+                                        config=self._cfg,
+                                        only_for_eval=True,
+                                        monitor=self._monitor)
+        # Extract the emulator model
+        if message.content[1:] is not None:
+            self.trainer.update(message.content[1] if len(message.content) == 2
+                                else message.content[1:],
+                                strict=self._cfg.federate.share_local_model)
+        if self.early_stopper.early_stopped and self._cfg.federate.method in [
+                "local", "global"
+        ]:
+            metrics = list(self.best_results.values())[0]
+        else:
+            metrics = {}
+            if self._cfg.finetune.before_eval:
+                self.trainer.finetune()
+            for split in self._cfg.eval.split:
+                # TODO: The time cost of evaluation is not considered here
+                emulator_eval_metrics = self.trainer.evaluate(
+                    target_data_split_name=split)
+                plugin_eval_matrics = raw_model_trainer.evaluate(
+                    target_data_split_name=split)
+
+                eval_metrics = {}
+                for key, value in emulator_eval_metrics.items():
+                    eval_metrics['emulator.' + key] = value
+                for key, value in plugin_eval_matrics.items():
+                    eval_metrics['plugin.' + key] = value
+
+                if self._cfg.federate.mode == 'distributed':
+                    logger.info(
+                        self._monitor.format_eval_res(eval_metrics,
+                                                      rnd=self.state,
+                                                      role='Client #{}'.format(
+                                                          self.ID),
+                                                      return_raw=True))
+
+                metrics.update(**eval_metrics)
+
+            formatted_eval_res = self._monitor.format_eval_res(
+                metrics,
+                rnd=self.state,
+                role='Client #{}'.format(self.ID),
+                forms=['raw'],
+                return_raw=True)
+            logger.info(formatted_eval_res)
+            self._monitor.update_best_result(self.best_results,
+                                             formatted_eval_res['Results_raw'],
+                                             results_type=f"client #{self.ID}")
+            self.history_results = merge_dict_of_results(
+                self.history_results, formatted_eval_res['Results_raw'])
+            self.early_stopper.track_and_check(self.history_results[
+                self._cfg.eval.best_res_update_round_wise_key])
+
+        self.comm_manager.send(
+            Message(msg_type='metrics',
+                    sender=self.ID,
+                    receiver=[sender],
+                    state=self.state,
+                    timestamp=timestamp,
+                    content=metrics))
--- federatedscope/llm/offsite_tuning/server.py
+++ federatedscope/llm/offsite_tuning/server.py
@@ -1,7 +1,9 @@
 import logging
 
 from federatedscope.core.message import Message
-from federatedscope.core.auxiliaries.utils import b64serializer
+from federatedscope.core.auxiliaries.utils import b64serializer, \
+    merge_dict_of_results
+from federatedscope.core.auxiliaries.trainer_builder import get_trainer
 from federatedscope.core.workers.server import Server
 
 from federatedscope.llm.offsite_tuning.utils import \
@@ -55,3 +57,73 @@ class OffsiteTuningServer(Server):
                     timestamp=self.cur_timestamp,
                     content=emulator_and_adapter))
         trigger_train_func(**kwargs_for_trigger_train_func)
+
+    def eval(self):
+        # Update the raw model with the new adaptors
+        self.raw_model.load_state_dict(self.model.state_dict(), strict=False)
+
+        if self._cfg.federate.make_global_eval:
+            # make the evaluation on raw model first
+            raw_model_trainer = get_trainer(model=self.raw_model,
+                                            data=self.data,
+                                            device=self.device,
+                                            config=self._cfg,
+                                            is_attacker=self.is_attacker,
+                                            monitor=self._monitor)
+            raw_metrics = {}
+            for split in self._cfg.eval.split:
+                metrics = raw_model_trainer.evaluate(
+                    target_data_split_name=split)
+                raw_metrics.update(**metrics)
+            for key, value in raw_metrics.items():
+                raw_metrics['plugin.' + key] = value
+
+            # By default, the evaluation is conducted one-by-one for all
+            # internal models;
+            # for other cases such as ensemble, override the eval function
+            for i in range(self.model_num):
+                trainer = self.trainers[i]
+                # Preform evaluation for emulator at server
+                metrics = {}
+                for split in self._cfg.eval.split:
+                    eval_metrics = trainer.evaluate(
+                        target_data_split_name=split)
+                    metrics.update(**eval_metrics)
+                for key, value in metrics.items():
+                    metrics['emulator.' + key] = value
+                metrics.update(**raw_metrics)
+                formatted_eval_res = self._monitor.format_eval_res(
+                    metrics,
+                    rnd=self.state,
+                    role='Server #',
+                    forms=self._cfg.eval.report,
+                    return_raw=self._cfg.federate.make_global_eval)
+                self._monitor.update_best_result(
+                    self.best_results,
+                    formatted_eval_res['Results_raw'],
+                    results_type="server_global_eval")
+                self.history_results = merge_dict_of_results(
+                    self.history_results, formatted_eval_res)
+                self._monitor.save_formatted_results(formatted_eval_res)
+                logger.info(formatted_eval_res)
+            self.check_and_save()
+        else:
+            # broadcast two models for evaluation
+            raw_model = b64serializer(self.raw_model, tool='dill')
+            skip_broadcast = self._cfg.federate.method in ["local", "global"]
+            model_para = [raw_model] + \
+                         [{} if skip_broadcast else model.state_dict()
+                          for model in self.models]
+
+            rnd = self.state - 1
+
+            self.comm_manager.send(
+                Message(msg_type='eval_offsite_tuning',
+                        sender=self.ID,
+                        receiver=list(self.comm_manager.neighbors.keys()),
+                        state=min(rnd, self.total_round_num),
+                        timestamp=self.cur_timestamp,
+                        content=model_para))
+            if self._cfg.federate.online_aggr:
+                for idx in range(self.model_num):
+                    self.aggregators[idx].reset()
