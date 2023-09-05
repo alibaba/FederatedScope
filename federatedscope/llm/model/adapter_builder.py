fix half precision for helm (#690)

federatedscope/llm/model/adapter_builder.py
@@ -211,7 +211,18 @@ class AdapterModel(nn.Module):
         Returns:
             The output of the model's generate method.
         """
-        return self.model.generate(*args, **kwargs)
+        try:
+            res = self.model.generate(*args, **kwargs)
+        except RuntimeError as e:
+            # When does evaluation in HELM,
+            # half precision will cause RuntimeError,
+            # the following solves it
+            if 'do_sample' in kwargs.keys():
+                del kwargs['do_sample']
+                res = self.model.generate(*args, **kwargs)
+            else:
+                raise RuntimeError(e)
+        return res
 
     def state_dict(self, return_trainable=True, *args, **kwargs):
         """