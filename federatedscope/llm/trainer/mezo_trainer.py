0aba27c92e117bfe51d59b1f8ff2d3eb0c606ea9
zhenqincn_intern <zhenqincn@gmail.com>
2023-09-05 19:37:18 +0800

    optimize code arrangement

--- federatedscope/llm/trainer/mezo_trainer.py
+++ federatedscope/llm/trainer/mezo_trainer.py
@@ -32,101 +32,99 @@ from federatedscope.core.trainers.enums import LIFECYCLE
 
 logger = logging.getLogger(__name__)
 
+
+def zo_step(ctx, zo_eps=1e-3):
+    """
+    Estimate gradient by MeZO. Return the loss from f(theta + z)
+    """
+    # determine which parameters to optimize 
+    ctx.named_parameters_to_optim = []
+    for name, param in ctx.model.named_parameters():
+        if param.requires_grad:
+            ctx.named_parameters_to_optim.append((name, param))
+
+    # Sample the random seed for sampling z
+    ctx.zo_random_seed = np.random.randint(1000000000)
+
+    # First function evaluation
+    zo_perturb_parameters(
+        ctx=ctx,
+        scaling_factor=1, 
+        zo_eps=zo_eps
+    )
+    logits1, loss1 = zo_forward(ctx)
+
+    # Second function evaluation
+    zo_perturb_parameters(
+        ctx=ctx,
+        scaling_factor=-2, 
+        zo_eps=zo_eps
+    )
+    _, loss2 = zo_forward(ctx)
+
+    ctx.projected_grad = ((loss1 - loss2) / (2 * zo_eps)).item()
+
+    # TODO double check here to confirm the implementation of 
+    # gradient accumulation in FS and MeZO
+
+    # Reset model back to its parameters at start of step
+    zo_perturb_parameters(
+        ctx=ctx,
+        scaling_factor=1,
+        zo_eps=zo_eps
+    )
+    return logits1, loss1
+
+
+def zo_perturb_parameters(ctx, scaling_factor=1, zo_eps=1e-3):
+    """
+    Perturb the parameters with random vector z.
+    Input: 
+    - random_seed: random seed for MeZO in-place perturbation 
+        (if it's None, we will use self.zo_random_seed)
+    - scaling_factor: theta = theta + scaling_factor * z * eps
+    """
+
+    # Set the random seed to ensure that we sample the same z for perturbation/update
+    torch.manual_seed(ctx.zo_random_seed)
+    
+    for _, param in ctx.named_parameters_to_optim:
+        z = torch.normal(
+            mean=0, 
+            std=1, 
+            size=param.data.size(), 
+            device=param.data.device, 
+            dtype=param.data.dtype
+        )
+        param.data = param.data + scaling_factor * z * zo_eps
+
+
+def zo_forward(ctx):
+    """
+    Get (no gradient) loss from the model. Dropout is turned off too.
+    """
+    ctx.model.eval()
+    # TODO support of non-differential functions
+    # if self.args.non_diff:
+    #     # Non-differentiable objective (may require autoregressive generation)
+    #     return self.zo_forward_nondiff(model, inputs)
+
+    with torch.inference_mode():
+        input_ids = ctx.data_batch['input_ids'].to(ctx.device)
+        labels = ctx.data_batch['labels'].to(ctx.device)
+        attention_mask = ctx.data_batch['attention_mask'].to(ctx.device)
+
+        outputs = ctx.model(input_ids=input_ids,
+                            labels=labels,
+                            attention_mask=attention_mask)
+        logits = outputs.logits
+        loss = outputs.loss
+    return logits.detach(), loss.detach()
+
+
 class MeZOTrainer(LLMTrainer):
-    @staticmethod
-    def zo_step(ctx, zo_eps=1e-3):
-        """
-        Estimate gradient by MeZO. Return the loss from f(theta + z)
-        """
-        # determine which parameters to optimize 
-        ctx.named_parameters_to_optim = []
-        for name, param in ctx.model.named_parameters():
-            if param.requires_grad:
-                ctx.named_parameters_to_optim.append((name, param))
-
-        # Sample the random seed for sampling z
-        ctx.zo_random_seed = np.random.randint(1000000000)
-
-        # First function evaluation
-        MeZOTrainer.zo_perturb_parameters(
-            ctx=ctx,
-            scaling_factor=1, 
-            zo_eps=zo_eps
-        )
-        logits1, loss1 = MeZOTrainer.zo_forward(ctx)
-
-        # Second function evaluation
-        MeZOTrainer.zo_perturb_parameters(
-            ctx=ctx,
-            scaling_factor=-2, 
-            zo_eps=zo_eps
-        )
-        _, loss2 = MeZOTrainer.zo_forward(ctx)
-
-        ctx.projected_grad = ((loss1 - loss2) / (2 * zo_eps)).item()
-
-        # No gradient accumulation support
-        # TODO double check here to confirm the implementation of gradient accumulation in FS and MeZO
-        # assert self.args.gradient_accumulation_steps == 1
-
-        # Reset model back to its parameters at start of step
-        MeZOTrainer.zo_perturb_parameters(
-            ctx=ctx,
-            scaling_factor=1,
-            zo_eps=zo_eps
-        )
-        return logits1, loss1
-    
-    
-    @staticmethod
-    def zo_perturb_parameters(ctx, scaling_factor=1, zo_eps=1e-3):
-        """
-        Perturb the parameters with random vector z.
-        Input: 
-        - random_seed: random seed for MeZO in-place perturbation (if it's None, we will use self.zo_random_seed)
-        - scaling_factor: theta = theta + scaling_factor * z * eps
-        """
-
-        # Set the random seed to ensure that we sample the same z for perturbation/update
-        torch.manual_seed(ctx.zo_random_seed)
-        
-        for name, param in ctx.named_parameters_to_optim:
-            z = torch.normal(
-                mean=0, 
-                std=1, 
-                size=param.data.size(), 
-                device=param.data.device, 
-                dtype=param.data.dtype
-            )
-            param.data = param.data + scaling_factor * z * zo_eps
-
-
-    @staticmethod
-    def zo_forward(ctx):
-        """
-        Get (no gradient) loss from the model. Dropout is turned off too.
-        """
-        ctx.model.eval()
-        # TODO support of non-differential functions
-        # if self.args.non_diff:
-        #     # Non-differentiable objective (may require autoregressive generation)
-        #     return self.zo_forward_nondiff(model, inputs)
-
-        with torch.inference_mode():
-            input_ids = ctx.data_batch['input_ids'].to(ctx.device)
-            labels = ctx.data_batch['labels'].to(ctx.device)
-            attention_mask = ctx.data_batch['attention_mask'].to(ctx.device)
-
-            outputs = ctx.model(input_ids=input_ids,
-                                labels=labels,
-                                attention_mask=attention_mask)
-            logits = outputs.logits
-            loss = outputs.loss
-        return logits.detach(), loss.detach()
-            
-            
     def _hook_on_batch_forward(self, ctx):
-        logits, loss = MeZOTrainer.zo_step(ctx)
+        logits, loss = zo_step(ctx)
         labels = ctx.data_batch['labels'].to(ctx.device)
         if torch.isnan(loss):
             ctx.skip_this_batch = CtxVar(True, LIFECYCLE.BATCH)
